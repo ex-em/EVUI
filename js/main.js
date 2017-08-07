418064655aa7fc415eaeadccbdea204510d5c197
Roaster Kay <overock@ex-em.com>
2017-08-07 11:01:21 +0900

    [#1] webpack-babel-demo로 scaffolding 변경

---
+++ js/main.js
@@ -0,0 +1,4 @@
+import 'babel-polyfill';
+import world from './world';
+
+document.getElementById('output').innerHTML = `Hello ${world}!`;
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,43 @@
+/* global __dirname */
+
+var path = require('path');
+
+var webpack = require('webpack');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+var dir_js = path.resolve(__dirname, 'js');
+var dir_html = path.resolve(__dirname, 'html');
+var dir_build = path.resolve(__dirname, 'build');
+
 module.exports = {
-  entry: './index.js',
-  output: {
-    path: './',
-    filename: 'bundle.js',
-    publicPath: ''
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?/,
+    entry: path.resolve(dir_js, 'main.js'),
+    output: {
+        path: dir_build,
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: dir_build,
+    },
+    module: {
         loaders: [
-          'babel'
-        ],
-        include: './index.js',
-        query: {
-          presets: [
-            'es2015'
-          ]
-        }
-      }
-    ]
-  }
-}
+            {
+                loader: 'babel-loader',
+                test: dir_js,
+            }
+        ]
+    },
+    plugins: [
+        // Simply copies the files over
+        new CopyWebpackPlugin([
+            { from: dir_html } // to: output.path
+        ]),
+        // Avoid publishing files when compilation fails
+        new webpack.NoErrorsPlugin()
+    ],
+    stats: {
+        // Nice colored output
+        colors: true
+    },
+    // Create Sourcemaps for the bundle
+    devtool: 'source-map',
+};
