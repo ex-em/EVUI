71e493dd9762265dd9fd61c82851c74e3f670a64
Roaster Kay <overock@ex-em.com>
2017-10-25 19:55:58 +0900

    [#10] computed methods 정의

--- src/vue/chart/bar/barchart.js
+++ src/vue/chart/bar/barchart.js
@@ -1,17 +1,41 @@
+import colgroup from 'colgroup.vue';
+
 export default {
     props: {
-
+        title: String,
+        columns: Array,
+        width: Number,
+        height: Number,
+        data: Array
     },
     data: function() {
-        return {};
+        return {
+        };
     },
     computed: {
+        range() {
+            let min = Infinity, max = -Infinity;
+            this.data.forEach(row => row.forEach(v => {
+                min = Math.min(min, v);
+                max = Math.max(max, v);
+            }));
 
+            return {
+                max: max,
+                min: min,
+                scale: this.height / (max - min)
+            };
+        },
+
+        cols() {
+            return this.columns.map((c, i) => this.data.map(v => v[i]));
+        }
     },
     methods: {
-
     },
     created: function() {
-
+    },
+    components: {
+        colgroup
     }
-};
+} ;
---
+++ src/vue/chart/bar/colgroup.vue
@@ -0,0 +1,3 @@
+<template src="./colgroup.html"></template>
+<script src="./colgroup.js"></script>
+<style scoped src="../chart.css"></style>
