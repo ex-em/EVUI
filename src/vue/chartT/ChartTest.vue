565f93ec6f4c86128be917d5b7072e03278c2191
Roaster Kay <overock@ex-em.com>
2017-11-15 17:44:40 +0900

    [#30] 12개 row를 표시하는 차트 테스트 업로

--- src/vue/chartT/ChartTest.vue
+++ src/vue/chartT/ChartTest.vue
@@ -20,49 +20,31 @@
 </template>
 <script>
     import chart from "./Chart.vue"
+    import mock0 from '../../mock/chart/0.json'
 
     export default {
         data() {
-            let xhr = new XMLHttpRequest();
-            xhr.open('GET', './mock/chart/0.json', false);
-            xhr.send(null);
-            if(xhr.status==200) {
-                let rslt = JSON.parse(xhr.responseText).map(v => {
-                    return {
-                        'Jan': v.n01,
-                        'Feb': v.n02,
-                        'Mar': v.n03,
-                        'Apr': v.n04,
-                        'May': v.n05,
-                        'Jun': v.n06,
-                        'Jul': v.n07,
-                        'Aug': v.n08,
-                        'Sep': v.n09,
-                        'Oct': v.n10,
-                        'Nov': v.n11,
-                        'Dec': v.n12
-                    };
-                });
-
-                return {
-                    Bar: {
-                        chartData: {
-                            series: rslt
+            let rslt = mock0.map(v => [v.n01, v.n02, v.n03, v.n04, v.n05, v.n06, v.n07, v.n08, v.n09, v.n10, v.n11, v.n12]);
+            console.log(rslt.slice(0, 10));
+            return {
+                Bar: {
+                    chartData: {
+                        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+                        series: rslt.slice(0, 12)
+                    },
+                    chartOptions: {
+                        type: "Bar",
+//                        width: "800px",
+                        height: "400px",
+                        isHorizontalBar: true,
+                        seriesBarDistance: 10,
+                        axisX: {
+                            showGrid: true,
+                            labelAlign: 'between',
                         },
-                        chartOptions: {
-                            type: "Bar",
-    //                        width: "800px",
-                            height: "400px",
-                            isHorizontalBar: false,
-                            seriesBarDistance: 10,
-                            axisX: {
-                                showGrid: true,
-                                labelAlign: 'between',
-                            },
-                            axisY: {
-                                divisor: 5,
-                                showGrid: true,
-                            }
+                        axisY: {
+                            divisor: 5,
+                            showGrid: true,
                         }
                     }
                 }
