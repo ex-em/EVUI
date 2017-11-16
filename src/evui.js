e8245306a3a1a1ea4adbcac7a9f432cc8ad67ef1
jwlee <jwlee@ex-em.com>
2017-11-16 15:32:45 +0900

    [#33] 빌드처리 관련 webpack 수정 ##########

--- src/evui.js
+++ src/evui.js
@@ -1,9 +1,11 @@
 import Chart from './vue/chartT/Chart.vue'
 import Grid from './vue/grid/Grid.vue'
+import Tree from './vue/tree/Tree.vue'
 
 export default {
     install (Vue, params = {}) {
         Vue.component('Chart', Chart);
         Vue.component('Grid', Grid);
+        Vue.component('Tree', Tree);
     }
 }
