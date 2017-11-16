8e62fed4023f5f54b6ef7b7e1b23b30c19112e04
HONGKYUN <daru87@ex-em.com>
2017-11-16 17:57:54 +0900

    [#39] 빌드 관련 파일 수정 ########## Browser 모드에서 사용할 수 있도록 수정

--- src/evui.js
+++ src/evui.js
@@ -2,10 +2,17 @@ import Chart from './vue/chartT/Chart.vue'
 import Grid from './vue/grid/Grid.vue'
 import Tree from './vue/tree/Tree.vue'
 
-export default {
-    install (Vue, params = {}) {
-        Vue.component('Chart', Chart);
-        Vue.component('Grid', Grid);
-        Vue.component('Tree', Tree);
-    }
+const EVUI = {};
+
+EVUI.install = function (Vue) {
+    Vue.component('Chart', Chart);
+    Vue.component('Grid', Grid);
+    Vue.component('Tree', Tree);
+};
+
+
+if (typeof window !== 'undefined' && window.Vue) {
+    window.Vue.use(EVUI)
 }
+
+export default EVUI;
