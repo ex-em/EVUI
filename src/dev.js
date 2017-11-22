8f17e239770a50f38a41c5a32145c9a21efe696b
HONGKYUN <daru87@ex-em.com>
2017-11-22 16:36:25 +0900

    [#43] 배포 관련 분리 및 수정 진행 ########## 1. npm run dev 와 npm run build 분리와 관련 파일 추가 2. component 배포 시 image 적용되지 않는 부분 Vue-Awesome 이용하여 수정

--- src/dev.js
+++ src/dev.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
+import Icon from 'vue-awesome/components/Icon.vue';
 import App from './vue/App.vue';
-
 import router from './router.js';
+
+Vue.use(Icon);
+Vue.component('icon', Icon);
 
 new Vue({
     el: '#app',
--- src/evui.js
+++ src/evui.js
@@ -1,10 +1,13 @@
 import Chart from './vue/chartT/Chart.vue'
 import Grid from './vue/grid/Grid.vue'
 import Tree from './vue/tree/Tree.vue'
-
+import 'vue-awesome/icons'
+import Icon from 'vue-awesome/components/Icon.vue';
 const EVUI = {};
 
 EVUI.install = function (Vue) {
+    Vue.use(Icon);
+    Vue.component('icon', Icon);
     Vue.component('Chart', Chart);
     Vue.component('Grid', Grid);
     Vue.component('Tree', Tree);
