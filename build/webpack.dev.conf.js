630debe038b6707b0aea3b93ea08798e9f5855f1
jwlee <jwlee@ex-em.com>
2018-02-02 18:04:14 +0900

    [#78] 2.0 환경 구축 ########## 중복된 resolve 설정 제거 (base.conf.js에 존재)

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -22,13 +22,6 @@ module.exports = merge(webpackBaseConfig, {
     publicPath: '/',
     filename: '[name].js',
   },
-  resolve: {
-    extensions: ['.js', '.vue', '.json'],
-    alias: {
-      'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src')
-    }
-  },
   devtool: '#cheap-module-eval-source-map',
   module: {
     rules:[
