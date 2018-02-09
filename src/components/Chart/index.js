d80e99df74a5f266ca04bb7439a6520f7a22f67a
jwlee <jwlee@ex-em.com>
2018-02-09 18:28:46 +0900

    [#84] Chart 개발 ########## 최초 작성

---
+++ src/components/Chart/index.js
@@ -0,0 +1,4 @@
+import Chart from './Chart';
+
+export default Chart;
+
---
+++ test/unit/specs/Chart/Chart.spec.js
@@ -0,0 +1,38 @@
+import { mount } from 'vue-test-utils';
+import Chart from '@/components/Chart/Chart';
+
+describe('Create Chart', () => {
+  const wrapper = mount(Chart);
+  const chart = wrapper.vm;
+
+  it('Mounted chart Vue Component', () => {
+    expect(chart.$el.getElementsByClassName('evui-chart-container')).to.exist;
+  });
+
+  it('Created Canvas Element', () => {
+    expect(wrapper.contains('canvas')).to.exist;
+  });
+
+
+
+
+  // it('is Exist Chart.vue file?', () => {
+  //   let vueFile = require('@/components/Chart/Chart');
+  //   expect(vueFile.default.name).to.be.equal('sample-canvas');
+  // });
+  //
+  // it('Create Canvas Object', () => {
+  //   const canvas = chart.find('#sample-canvas');
+  //   expect(canvas.contains('canvas')).toBe(true);
+  // });
+  //
+  // it('Check Browser support', () => {
+  //   expect(chart.vm.checkBrowserSupport).to.exist;
+  // });
+  //
+  // it('Check typeof Browser support', () => {
+  //   expect(typeof chart.vm.checkBrowserSupport).to.equal('function');
+  // });
+
+
+});
