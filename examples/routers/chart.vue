5d04d7777a6e8f849d1adf8330a889684cbb0aa2
jwlee <jwlee@ex-em.com>
2018-06-05 13:08:13 +0900

    [#84] 2.0 Chart 개발 - Examples 수정 ########## Examples 수정 및 BarChart category가 없을 시에 대한 예외 처리 추가.

--- examples/routers/chart.vue
+++ examples/routers/chart.vue
@@ -1,134 +1,23 @@
 <template>
-  <div style="width: 100%; height: 100%;">
-    <div style="width: 400px; height: 400px; position: absolute;">
-      <chart
-        :data="chartData"
-        :options="chartOptions"
-      />
-    </div>
+  <div>
+    <line-chart/>
+    <bar-chart/>
+    <pie-chart/>
   </div>
 </template>
 <script>
-  import chart from '@/components/chart';
+  import LineChart from './charts/chart.line';
+  import BarChart from './charts/chart.bar';
+  import PieChart from './charts/chart.pie';
 
   export default {
     components: {
-      chart,
+      LineChart,
+      BarChart,
+      PieChart,
     },
     data() {
       return {
-        chartData: {
-          series: [
-            {
-              id: 'series_0',
-              name: 'series_0',
-              show: true,
-              data: 15,
-              children: [
-                {
-                  id: 'series_0_0',
-                  name: 'series_0_0',
-                  show: true,
-                  data: 10,
-                },
-                {
-                  id: 'series_0_1',
-                  name: 'series_0_1',
-                  show: true,
-                  data: 12,
-                },
-              ],
-            },
-            {
-              id: 'series_1',
-              name: 'series_1',
-              show: true,
-              data: 10,
-              children: [
-                {
-                  id: 'series_1_0',
-                  name: 'series_1_0',
-                  show: true,
-                  data: 17,
-                },
-                {
-                  id: 'series_1_1',
-                  name: 'series_1_1',
-                  show: true,
-                  data: 13,
-                  children: [
-                    {
-                      id: 'series_1_1_0',
-                      name: 'series_1_1_0',
-                      show: true,
-                      data: 17,
-                    },
-                    {
-                      id: 'series_1_1_1',
-                      name: 'series_1_1_1',
-                      show: true,
-                      data: 10,
-                    },
-                  ],
-                },
-                {
-                  id: 'series_1_2',
-                  name: 'series_1_3',
-                  show: true,
-                  data: 30,
-                  children: [
-                    {
-                      id: 'series_1_2_0',
-                      name: 'series_1_2_0',
-                      show: true,
-                      data: 17,
-                      children: [
-                        {
-                          id: 'series_1_2_0',
-                          name: 'series_1_2_0',
-                          show: true,
-                          data: 10,
-                        },
-                        {
-                          id: 'series_1_2_0',
-                          name: 'series_1_2_0',
-                          show: true,
-                          data: 10,
-                        },
-                        {
-                          id: 'series_1_2_0',
-                          name: 'series_1_2_0',
-                          show: true,
-                          data: 15,
-                        },
-                      ],
-                    },
-                  ],
-                },
-              ],
-            },
-            {
-              id: 'series_2',
-              name: 'series_2',
-              show: true,
-              data: 12,
-            },
-          ],
-        },
-        chartOptions: {
-          type: 'sunburst',
-          width: '100%',
-          height: '100%',
-          border: 2,
-          doughnutHoleSize: 0.4,
-          title: {
-            text: 'EVUI CHART 2.0',
-            style: '20px Droid Sans',
-            color: '#000000',
-            height: 50,
-            show: true,
-          },
-        },
       };
     },
   };
