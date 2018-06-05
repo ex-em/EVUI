748b6d0f36bcd9603f92de6a8337479c90c92cb6
jwlee <jwlee@ex-em.com>
2018-06-05 13:08:13 +0900

    [#84] 2.0 Chart 개발 - Examples 수정 ########## Examples 수정 및 BarChart category가 없을 시에 대한 예외 처리 추가.

---
+++ examples/routers/charts/chart.pie.vue
@@ -0,0 +1,182 @@
+<template>
+  <div style="width: 100%; height: 100%; display: inline-flex;">
+    <chart
+      :data="pieData"
+      :options="pieOptions"
+    />
+    <chart
+      :data="pieData"
+      :options="doughnutOptions"
+    />
+    <chart
+      :data="sunburstData"
+      :options="sunburstOptions"
+    />
+  </div>
+</template>
+<script>
+  import chart from '@/components/chart';
+
+  export default {
+    components: {
+      chart,
+    },
+    data() {
+      return {
+        pieData: {
+          series: [
+            {
+              id: 'series0',
+              name: 'series-0',
+              show: true,
+              xAxisIndex: 0,
+              yAxisIndex: 0,
+              data: [5, 10, 6],
+            },
+            {
+              id: 'series1',
+              name: 'series-1',
+              show: true,
+              xAxisIndex: 0,
+              yAxisIndex: 0,
+              data: [10, 12, 8],
+            },
+            {
+              id: 'series2',
+              name: 'series-2',
+              show: true,
+              xAxisIndex: 0,
+              yAxisIndex: 0,
+              data: [15, 10, 20],
+            },
+            {
+              id: 'series3',
+              name: 'series-3',
+              show: true,
+              xAxisIndex: 0,
+              yAxisIndex: 0,
+              data: [5, 15, 10],
+            },
+          ],
+        },
+        sunburstData: {
+          series: [
+            {
+              id: 'series_0',
+              name: 'series_0',
+              show: true,
+              data: 15,
+              children: [
+                {
+                  id: 'series_0_0',
+                  name: 'series_0_0',
+                  show: true,
+                  data: 10,
+                },
+                {
+                  id: 'series_0_1',
+                  name: 'series_0_1',
+                  show: true,
+                  data: 12,
+                },
+              ],
+            },
+            {
+              id: 'series_1',
+              name: 'series_1',
+              show: true,
+              data: 10,
+              children: [
+                {
+                  id: 'series_1_0',
+                  name: 'series_1_0',
+                  show: true,
+                  data: 17,
+                },
+                {
+                  id: 'series_1_1',
+                  name: 'series_1_1',
+                  show: true,
+                  data: 13,
+                  children: [
+                    {
+                      id: 'series_1_1_0',
+                      name: 'series_1_1_0',
+                      show: true,
+                      data: 17,
+                    },
+                    {
+                      id: 'series_1_1_1',
+                      name: 'series_1_1_1',
+                      show: true,
+                      data: 10,
+                    },
+                  ],
+                },
+                {
+                  id: 'series_1_2',
+                  name: 'series_1_3',
+                  show: true,
+                  data: 30,
+                  children: [
+                    {
+                      id: 'series_1_2_0',
+                      name: 'series_1_2_0',
+                      show: true,
+                      data: 17,
+                      children: [
+                        {
+                          id: 'series_1_2_0',
+                          name: 'series_1_2_0',
+                          show: true,
+                          data: 10,
+                        },
+                        {
+                          id: 'series_1_2_0',
+                          name: 'series_1_2_0',
+                          show: true,
+                          data: 10,
+                        },
+                        {
+                          id: 'series_1_2_0',
+                          name: 'series_1_2_0',
+                          show: true,
+                          data: 15,
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              id: 'series_2',
+              name: 'series_2',
+              show: true,
+              data: 12,
+            },
+          ],
+        },
+        pieOptions: {
+          type: 'pie',
+          width: '400px',
+          height: '350px',
+        },
+        doughnutOptions: {
+          type: 'pie',
+          doughnutHoleSize: 0.4,
+          width: '400px',
+          height: '350px',
+        },
+        sunburstOptions: {
+          type: 'sunburst',
+          width: '400px',
+          height: '350px',
+          doughnutHoleSize: 0.4,
+        },
+      };
+    },
+  };
+</script>
+<style>
+</style>
--- src/components/chart/charts/chart.bar.js
+++ src/components/chart/charts/chart.bar.js
@@ -27,7 +27,7 @@ export default class BarChart extends BaseChart {
   drawSeries(seriesIndex) {
     // 해당 series 정보 및 ctx 등 확인
     const series = this.seriesList[seriesIndex];
-    const category = this.data.category;
+    const category = this.data.category || [];
     const ctx = this.bufferCtx;
     // series에 특정한 color 값이 없다면, options의 colors 참조
 
@@ -37,7 +37,7 @@ export default class BarChart extends BaseChart {
 
     // bar 차트는 category가 중심이 되며 축의 넓이를 category의 수로 나눠 각 cateogory의 넓이를 구함.
     const drawingArea = this.options.horizontal ? this.drawingYArea() : this.drawingXArea();
-    const categoryArea = drawingArea / category.length;
+    const categoryArea = drawingArea / (category.length || 1);
     // category간 padding값 (left, right)
     const categoryPadding = 2;
     let barArea;
