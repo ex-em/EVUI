ae2bf6c4e6db74ba4a6df653e4206684d35daca3
kang <kangjm103@naver.com>
2018-07-23 14:25:39 +0900

    [#123] Button examples page 추가

---
+++ home/guide/views/examples/button/button-size.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="btn-size">
+    <Button
+      v-for="btn in btnList"
+      :key="btn.name"
+      :name="btn.name"
+      :text="btn.text"
+      :type="btn.type"
+      :size="btn.size"
+      :btn-style="btn.btnStyle"
+    />
+  </div>
+</template>
+
+<script>
+  import '@/styles/evui.css';
+  import Button from '@/components/button';
+
+  export default {
+    components: {
+      Button,
+    },
+    data() {
+      return {
+        btnList: [],
+      };
+    },
+    mounted() {
+      this.btnList = this.getBtnList();
+    },
+    methods: {
+      getBtnList() {
+        let btn;
+        const btnList = [
+          { text: 'small', name: 'smallBtn', type: 'primary', size: 'small' },
+          { text: 'normal', name: 'normalBtn', type: 'primary', size: 'normal' },
+          { text: 'large', name: 'largeBtn', type: 'primary', size: 'large' },
+        ];
+
+        for (let ix = 0, ixLen = btnList.length; ix < ixLen; ix++) {
+          btn = btnList[ix];
+
+          if (ix > 0) {
+            btn.btnStyle = {
+              marginLeft: '8px',
+            };
+          }
+        }
+
+        return btnList;
+      },
+    },
+  };
+</script>
+
+<style scoped>
+  .btn-size{
+    display: inline-block;
+    margin-left: 5px;
+    height: 40px;
+  }
+</style>
