7cb691003889a024224d4e8bff50c5e25d9421ac
Jun <wnsrl56@gmail.com>
2018-07-26 15:15:12 +0900

    [#163] code component description 높이 문제 수정 ###### - description 높이를 계산해줘서 expand / hide 되도록 수정함

--- home/guide/views/examples/inputnumber/input-number-view.vue
+++ home/guide/views/examples/inputnumber/input-number-view.vue
@@ -13,7 +13,6 @@
         <code-view
           :code-url="item.url"
           :description="item.description"
-          :is-bottom="true"
         >
           <component :is="item.component"/>
         </code-view>
@@ -37,11 +36,7 @@
           {
             url: './guide/views/examples/inputnumber/input-number-default.vue',
             component: inputNumberDefault,
-            description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                          culpa qui officia deserunt mollit anim id est laborum.`,
-            isBottom: true,
+            description: 'Test Code',
             title: 'Default',
           },
         ],
