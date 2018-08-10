4fde24878cec9da1321b97ebabe666ab03e4ec80
Kang <kangjm103@ex-em.com>
2018-08-10 16:54:34 +0900

    [#153] Slider 버그 수정 ############################################## - component tag name ev 로 수정 - width prop 추가 - drag 시, 버튼 크기 효과 유지

--- home/guide/views/examples/slider/slider-default.vue
+++ home/guide/views/examples/slider/slider-default.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="slider-default">
-    <Slider :value="70"/>
+    <ev-slider
+      :width="200"
+      :value="70"
+    />
   </div>
 </template>
 
 <script>
-  import '@/styles/evui.css';
-  import Slider from '@/components/slider';
-
   export default {
     name: 'SliderDefault',
-    components: {
-      Slider,
-    },
   };
 </script>
 
