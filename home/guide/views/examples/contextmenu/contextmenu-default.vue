d193fe80161bf6dfea92bb0a7db795410c4f24c0
Kang <kangjm103@gmail.com>
2018-09-19 15:45:25 +0900

    [#200] contextmenu > recursive 를 이용하여 sub menu 구현

--- home/guide/views/examples/contextmenu/contextmenu-default.vue
+++ home/guide/views/examples/contextmenu/contextmenu-default.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
-    <div style="height: 40px; margin-top: 20px">컨텍스트 메뉴를 보려면 마우스 우클릭 하세요.<br></div>
+    <div class="contextmenu-default">컨텍스트 메뉴를 보려면 마우스 우클릭 하세요. (사용 여부 옵션: is-use)<br></div>
     <div
-      class="menu-context-default"
+      class="contextmenu-default-body"
       @contextmenu="onContextMenu"
     />
     <ev-context-menu
       ref="contextMenu"
       :items="items"
+      @click="onClick"
     />
   </div>
 </template>
@@ -16,47 +17,86 @@
   export default {
     data() {
       return {
-        items: [
-          {
-            text: 'menu1',
-            fn: this.onClickMenu,
-          },
-          {
-            text: 'menu2',
-            fn: this.onClickMenu,
-          },
-          {
-            text: 'menu3',
-            fn: this.onClickMenu,
-          },
-          {
-            text: 'menu4',
-            fn: this.onClickMenu,
-          },
-          {
-            text: 'menu5',
-            fn: this.onClickMenu,
-          },
-        ],
+        items: [],
       };
+    },
+    created() {
+      this.items = this.getItems();
     },
     methods: {
       onContextMenu(e) {
         const contextMenu = this.$refs.contextMenu;
-        // body 에 붙이기 전 임시 위치 조정
         contextMenu.setPosition(e.pageX - 277, e.pageY - 233);
-        // contextMenu.setPosition(e.pageX, e.pageY);
         contextMenu.show();
         e.preventDefault();
       },
-      onClickMenu() {
+      onClick(item) {
+        console.log(`text: ${item.text} / itemId: ${item.itemId} / depth: ${item.depth} / rowIdx: ${item.rowIdx} / parentRowIdx: ${item.parentRowIdx}`);
+      },
+      getItems() {
+        return [
+          {
+            text: 'menu(1)',
+            itemId: 'menu1',
+          },
+          {
+            text: 'menu(2)',
+            itemId: 'menu2',
+            items: [
+              {
+                text: 'menu(2-1)',
+                itemId: 'menu2_1',
+                items: [
+                  {
+                    text: 'menu(2-1-1)',
+                    itemId: 'menu2_1_1',
+                  },
+                  {
+                    text: 'menu(2-1-2)',
+                    itemId: 'menu2_1_2',
+                  },
+                  {
+                    text: 'menu(2-1-3)',
+                    itemId: 'menu2_1_3',
+                  },
+                ],
+              },
+              {
+                text: 'menu(2-2)',
+                itemId: 'menu2_2',
+              },
+            ],
+          },
+          {
+            text: 'menu(3)',
+            itemId: 'menu3',
+          },
+          {
+            text: 'menu(4)',
+            itemId: 'menu4',
+            items: [
+              {
+                text: 'menu(4-1)',
+                itemId: 'menu4',
+              },
+            ],
+          },
+          {
+            text: 'menu(5)',
+            itemId: 'menu5',
+          },
+        ];
       },
     },
   };
 </script>
 
 <style scoped>
-  .menu-context-default{
+  .contextmenu-default{
+    height: 40px;
+    margin-top: 20px;
+  }
+  .contextmenu-default-body{
     width: 800px;
     height: 500px;
     background: #8D99A7;
--- src/components/menu/menu.context.vue
+++ src/components/menu/menu.context.vue
@@ -1,29 +1,30 @@
 <template>
   <div
     v-click-outside="hide"
-    v-if="isShow"
+    v-show="isShow"
     :style="ctxMenuStyle"
     :class="prefixEvui"
   >
-    <div :class="`${prefixEvui}-separator`"/>
-    <div
-      v-for="(item, idx) in items"
-      :key="item.text"
-      :class="`${prefixEvui}-row`"
-      @click="onClick($event, item, idx)"
-    >
-      {{ item.text }}
-    </div>
+    <Context-children
+      ref="ctxChildren"
+      :depth="0"
+      :items="items"
+      @click="onClick"
+    />
   </div>
 </template>
 
 <script>
   import '@/styles/evui.css';
+  import ContextChildren from '@/components/menu/context.children';
 
   const prefixEvui = 'ev-contextmenu';
 
   export default {
     name: 'ContextMenu',
+    components: {
+      ContextChildren,
+    },
     directives: {
       'click-outside': {
         bind(el, binding) {
@@ -46,7 +47,7 @@
       },
     },
     props: {
-      useCtxMenu: {
+      isUse: {
         type: Boolean,
         default: true,
       },
@@ -64,39 +65,48 @@
           return [];
         },
         validator(value) {
-          return typeof value === 'object' || value !== null;
+          return value != null && value.constructor === Array;
         },
       },
     },
     data() {
       return {
         prefixEvui,
-        isShow: false,
         top: 0,
         left: 0,
+        isShow: false,
       };
     },
     computed: {
       ctxMenuStyle() {
-        return `top: ${this.top}px; left: ${this.left}px`;
+        return `top: ${this.top}px; left: ${this.left}px;`;
       },
     },
     created() {
       this.setPosition(this.x, this.y);
     },
-    mounted() {
-    },
     methods: {
-      onClick(e, item, idx) {
-        if (item.fn) {
-          item.fn(e, item.text, idx);
-        }
+      onClick(item) {
+        this.$emit('click', item);
       },
       setPosition(x, y) {
-        this.top = `${this.removePixel(y)}`;
-        this.left = `${this.removePixel(x)}`;
+        this.top = `${this.extractNumber(y)}`;
+        this.left = `${this.extractNumber(x)}`;
       },
-      removePixel(input) {
+      show() {
+        if (this.isUse) {
+          this.isShow = true;
+        }
+      },
+      hide() {
+        this.isShow = false;
+        const ctxChildren = this.$refs.ctxChildren;
+
+        if (ctxChildren && ctxChildren.clearSubMenuKey) {
+          ctxChildren.clearSubMenuKey(this.$children);
+        }
+      },
+      extractNumber(input) {
         let result;
 
         if (typeof input === 'string' && input) {
@@ -110,52 +120,13 @@
 
         return result || 0;
       },
-      show() {
-        if (this.useCtxMenu) {
-          this.isShow = true;
-        }
-      },
-      hide() {
-        this.isShow = false;
-      },
     },
   };
 </script>
 
-<style scoped>
+<style>
   .ev-contextmenu{
     position: absolute;
-    padding: 2px;
-    background: #f0f0f0;
-    border: 1px solid #d0d0d0;
-    font-size: 12px;
-    color: #222222;
     z-index: 9999;
   }
-  .ev-contextmenu-separator {
-    position: absolute;
-    top: 0;
-    left: 24px;
-    width: 2px;
-    height: calc(100% - 4px);
-    margin: 2px 0 2px 0;
-    background-color: white;
-    border-left: solid 1px #e0e0e0;
-    overflow: hidden;
-  }
-  .ev-contextmenu-row{
-    position: relative;
-    padding: 2px 12px 2px 34px;
-    line-height: 22px;
-    white-space: nowrap;
-    overflow: hidden;
-    cursor: pointer;
-  }
-  .ev-contextmenu-row:hover{
-    padding: 1px 11px 1px 33px;
-    background-color: #e6e6e6;
-    border: 1px solid #9d9d9d;
-    border-radius: 3px;
-  }
-
 </style>
