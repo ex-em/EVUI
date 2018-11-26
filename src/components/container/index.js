208e2cfacd48a8e4decf7804812bf6d8c2561157
jwlee <jwlee@ex-em.com>
2018-11-26 10:59:25 +0900

    [#264] common quantity 변경
    ##########
     - 사용하지 않는 코드 제거
     - common.utils에 getQuantity 함수 추가.

--- src/components/container/index.js
+++
@@ -1,7 +0,0 @@
-import mainContainer from './container.main';
-import ResizeBar from './container.spliter';
-import boxPanel from './container.panel';
-// Default styling
-import './container.css';
-
-export { mainContainer, ResizeBar, boxPanel };
