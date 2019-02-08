b208f412274a0edc81fdcabb3ecac8e455ca1014
kdeun1 <kdeun1@ex-em.com>
2019-02-08 09:35:01 +0900

    [#237] radio size속성 추가, css 수정 (#292)
    * [#237] Radio css 적용
    ###########################
    - 라디오 박스 <label> 태그 위치 변경
    - css 가상요소 :before, :after를 사용하여 기존 radio 태그 스타일이 아니고 제품과 유사한 스타일로 변경
    
    * [#237] Radio text vertical middle css apply
    ###########################
    - 라디오 박스 <label> 내부 슬롯에 있는 text가 vertical middle할 수 있도록 css 수정
    
    * [#237] Radio type "small" 추가
    ###########################
    - 라디오 박스 타입에 small을 추가하여 작은 타입을 선택할 수 있도록 함
    - disabled + selected 상태인 경우 올바른 색상이 나오지 않는 버그 수정
    
    * [#237] Radio label background transparent apply
    ###########################
    
    * [#237] Change Radio attr-name, Change Guide example, ADD Radio-Group Attr "size"
    ###########################
    - Attr-name "type"을 "size"로 변경
      - size 'small' 추가
    - Radio 가이드 예제 수정
    - <ev-radio>, <ev-radio-group>에 모두 size=`'small'`속성 추가
    
    * [#237] radio size attr 자식, 부모 모두 사용할 수 있도록 코드 수정
    ###########################

--- home/guide/views/examples/radio/radio-group-default.vue
+++ home/guide/views/examples/radio/radio-group-default.vue
@@ -30,6 +30,39 @@
     bind value : {{ bindingValue }}
     <br>
     <br>
+    <h5># Default Model small size</h5>
+    <br>
+    <RadioGroup
+      v-model="bindingValue2"
+      :size="`small`"
+      @change-event="changeEvent"
+    >
+      <Radio
+        :value="radioObj2[0].value"
+      >
+        {{ radioObj2[0].text }}
+      </Radio>
+      <Radio
+        :value="radioObj2[1].value"
+        :disabled="true"
+        class="radioStyle"
+      >
+        {{ radioObj2[1].text }}
+      </Radio>
+      <Radio
+        :value="radioObj2[2].value"
+        class="radioStyle"
+      >
+        {{ radioObj2[2].text }}
+      </Radio>
+    </RadioGroup>
+    <br>
+    <br>
+    bind value : {{ bindingValue2 }}
+    <br>
+    <br>
+    <br>
+    <br>
   </div>
 </template>
 
@@ -46,6 +79,21 @@
       return {
         bindingValue: 'cellPhone',
         radioObj: [
+          {
+            text: 'Iphone',
+            value: 'cellPhone',
+          },
+          {
+            text: 'Hermes',
+            value: 'imitation',
+          },
+          {
+            text: 'Apple Watch',
+            value: 'luxury',
+          },
+        ],
+        bindingValue2: 'cellPhone',
+        radioObj2: [
           {
             text: 'Iphone',
             value: 'cellPhone',
