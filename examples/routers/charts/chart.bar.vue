4ca08ad83a243db91f4dba926f03e127f3599e43
jwlee <jwlee@ex-em.com>
2019-02-21 15:08:54 +0900

    [#84] Chart 리팩토링 ########## - bar sample 변경

--- examples/routers/charts/chart.bar.vue
+++ examples/routers/charts/chart.bar.vue
@@ -17,43 +17,18 @@
         defaultData: {
           series: {
             series1: { name: 'series#1', show: true, type: 'bar', fill: true, point: true },
-            // series2: { name: 'series#2', show: true, type: 'bar', fill: true, point: true },
-            // series3: { name: 'series#3', show: true, type: 'bar', fill: true, point: true },
           },
           groups: [
           ],
-          // labels: [
-          //   'aeeeeeeeeeee',
-          //   'b',
-          //   'c',
-          //   'd',
-          //   'e',
-          // ],
           labels: [
-            +new Date('2017/01/01 00:00:00'),
-            +new Date('2017/01/01 00:01:00'),
-            +new Date('2017/01/01 00:02:00'),
-            +new Date('2017/01/01 00:03:00'),
-            +new Date('2017/01/01 00:04:00'),
+            'aeeeeeeeeeee',
+            'b',
+            'c',
+            'd',
+            'e',
           ],
           data: {
             series1: [100, 150, 51, 150, 350],
-            // series2: [200, 50, 100, 300, 150],
-            // series3: [100, 200, 300, 100, 401],
-            // series3: [
-            //   { x: 'b', y: 100 },
-            //   { x: 'c', y: 200 },
-            //   { x: 'd', y: 300 },
-            //   { x: 'e', y: 100 },
-            //   { x: 'f', y: 401 },
-            // ],
-            series3: [
-              // { x: 100, y: 'b' },
-              // { x: 200, y: 'c' },
-              // { x: 300, y: 'd' },
-              // { x: 100, y: 'e' },
-              // { x: 401, y: 'f' },
-            ],
           },
         },
         lineOptions: {
@@ -69,14 +44,9 @@
             position: 'right',
           },
           horizontal: false,
-          // axesX: [{
-          //   type: 'step',
-          //   showGrid: false,
-          // }],
           axesX: [{
-            type: 'time',
-            timeFormat: 'HH:mm:ss',
-            interval: 'minute',
+            type: 'step',
+            showGrid: false,
           }],
           axesY: [{
             type: 'linear',
