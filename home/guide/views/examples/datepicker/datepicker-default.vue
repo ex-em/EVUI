18ab519f9ccb200d8e242227f4cabda6fd5016bf
kdeun1 <kdeun1@ex-em.com>
2019-04-09 10:17:14 +0900

    [#343] datepicker :size="'fit'" 추가, calendar.core.js 좌표 로직 수정 (#344)
    ###########################
    - datepicker :size="'fit'" 추가
    - calendar.core.js 좌표 로직 수정

--- home/guide/views/examples/datepicker/datepicker-default.vue
+++ home/guide/views/examples/datepicker/datepicker-default.vue
@@ -8,6 +8,12 @@
       :options="option"
       class="evui-datepicker-temp"
     />
+    <datepicker
+      v-model="bindDay2"
+      :options="option2"
+      :size="'fit'"
+      class="evui-datepicker-temp"
+    />
   </div>
 </template>
 
@@ -23,6 +29,11 @@
       return {
         bindDay: '2018-07-09',
         option: {
+          selectDayType: 'day',
+          initSelectDayFlag: true,
+        },
+        bindDay2: '2019-04-09',
+        option2: {
           selectDayType: 'day',
           initSelectDayFlag: true,
         },
