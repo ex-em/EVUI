6192d2b722ee6efc78894e08cb6fb45e5412fe55
jykim <jykim@ex-em.com>
2019-11-12 17:21:29 +0900

    [#384] Table refactoring 1. 초기 로딩 시 목록 표시 문제 수정 2. url 직접 접근에 대한 리다이렉션 처리 3. Dom 리사이즈 시 컬럼 사이즈 재 계산 안되는 문제 4. 컬럼 show/hide 기능 5. 이벤트 처리 (row click, checked, selected)

--- src/components/grid/grid.vue
+++ src/components/grid/grid.vue
@@ -1,5 +1,6 @@
 <template>
   <div
+    v-resize="onResize"
     v-cloak
     :class="getTableClass"
   >
@@ -9,7 +10,20 @@
     >
       <ul class="column-list">
         <li
+          v-if="useCheckbox.use"
+          style="width: 30px;"
+          class="column"
+        >
+          <ev-checkbox
+            v-if="isHeaderCheckBox"
+            v-model="isHeaderChecked"
+            :type="`square`"
+            @on-click="onCheckAll"
+          />
+        </li>
+        <li
           v-for="(column, index) in orderedColumns"
+          v-show="!column.hide"
           :key="index"
           :style="`width: ${column.width}px;`"
           class="column"
@@ -47,13 +61,30 @@
           <tr
             v-for="(row, rowIndex) in viewStore"
             :key="rowIndex"
+            :class="row[2] === selected ? 'selected' : ''"
+            @click="onRowClick($event, row)"
           >
             <td
-              v-for="(column, viewIndex) in orderedColumns"
-              :key="viewIndex"
-              :style="`width: ${column.width}px; height: ${rowHeight}px;`"
+              v-if="useCheckbox.use"
+              style="width: 30px;"
             >
-              {{ row[column.index] }}
+              <ev-checkbox
+                v-model="row[1]"
+                :type="`square`"
+                @on-click="onCheck($event, row)"
+                @click.native.stop=""
+              />
+            </td>
+            <td
+              v-for="(column, viewIndex) in orderedColumns"
+              v-show="!column.hide"
+              :key="viewIndex"
+              :data-name="column.field"
+              :data-index="column.index"
+              :style="
+              `width: ${column.width}px; height: ${rowHeight}px; line-height: ${rowHeight}px`"
+            >
+              {{ row[2][column.index] }}
             </td>
           </tr>
         </tbody>
@@ -72,11 +103,19 @@
   </div>
 </template>
 <script>
+  import resize from 'vue-resize-directive';
   import _ from 'lodash-es';
   import TableFilter from './grid.filter';
 
+  const ROW_INDEX = 0;
+  const ROW_CHECK_INDEX = 1;
+  const ROW_DATA_INDEX = 2;
+
   export default {
     name: 'EvTableNew',
+    directives: {
+      resize,
+    },
     components: {
       TableFilter,
     },
@@ -89,9 +128,17 @@
         type: Array,
         default: () => [],
       },
-      adjust: {
-        type: Boolean,
-        default: true,
+      selected: {
+        type: Array,
+        default: () => [],
+      },
+      checked: {
+        type: Array,
+        default: () => [],
+      },
+      option: {
+        type: Object,
+        default: () => {},
       },
     },
     data() {
@@ -102,8 +149,11 @@
         orderedColumns: [],
         sortOrder: 'desc',
         sortField: '',
-        rowHeight: 24,
-        columnWidth: 80,
+        adjust: this.option.adjust || false,
+        useSelect: this.option.useSelect || true,
+        useCheckbox: this.option.useCheckbox || {},
+        rowHeight: this.option.rowHeight || 24,
+        columnWidth: this.option.columnWidth || 80,
         lastScroll: {},
         vScrollTopHeight: 0,
         vScrollBottomHeight: 0,
@@ -112,6 +162,9 @@
         sortList: {},
         filterList: {},
         filterCondition: {},
+        checkedRows: this.checked.map(item => item),
+        prevCheckedRow: [],
+        isHeaderChecked: false,
       };
     },
     computed: {
@@ -124,16 +177,21 @@
       },
       useFilter() {
         let result = false;
-        const columns = Object.keys(this.filterList);
+        const columns = Object.keys(this.filterList || {});
 
         for (let ix = 0; ix < columns.length; ix++) {
-            if (this.filterList[columns[ix]].use) {
-                result = true;
-                break;
-            }
+          if (this.filterList[columns[ix]].use) {
+            result = true;
+            break;
+          }
         }
 
         return result;
+      },
+      isHeaderCheckBox() {
+        const option = this.useCheckbox;
+
+        return option.use && option.headerCheck && option.mode !== 'single';
       },
     },
     watch: {
@@ -158,36 +216,11 @@
       this.orderedColumns = this.columns.map((column, index) => ({ index, ...column }));
     },
     mounted() {
-      let columnWidth = this.columnWidth;
-      if (this.adjust) {
-        const el = this.$refs.body;
-        const elWidth = el.clientWidth;
-        const result = this.orderedColumns.reduce((acc, column) => {
-          if (column.width) {
-            acc.totalWidth += column.width;
-          } else {
-            acc.emptyCount++;
-          }
-
-          return acc;
-        }, { totalWidth: 0, emptyCount: 0 });
-
-        columnWidth = elWidth - result.totalWidth;
-        columnWidth = columnWidth > -1 ? columnWidth / result.emptyCount : this.columnWidth;
-        columnWidth = columnWidth < 100 ? 100 : columnWidth;
-
-        this.columnWidth = columnWidth;
-      }
-
-      this.orderedColumns.map((column) => {
-        const item = column;
-        if (!item.width) {
-          item.width = columnWidth;
-        }
-        return item;
-      });
-
-      this.$forceUpdate();
+      setTimeout(() => {
+        this.calculatedColumn();
+        this.setStore(this.rows);
+        this.$forceUpdate();
+      }, 0);
     },
     methods: {
       getColumnName(field) {
@@ -195,6 +228,55 @@
       },
       getColumnIndex(field) {
         return this.columns.findIndex(column => column.field === field);
+      },
+      calculatedColumn() {
+        let columnWidth = this.columnWidth;
+        let remainWidth = 0;
+        if (this.adjust) {
+          const el = this.$refs.body;
+          let elWidth = el.clientWidth;
+          const elHeight = el.clientHeight;
+          const result = this.orderedColumns.reduce((acc, column) => {
+            if (column.hide) {
+              return acc;
+            }
+
+            if (column.width) {
+              acc.totalWidth += column.width;
+            } else {
+              acc.emptyCount++;
+            }
+
+            return acc;
+          }, { totalWidth: 0, emptyCount: 0 });
+
+          if (this.rowHeight * this.rows.length > elHeight) {
+            elWidth += 17;
+          }
+
+          columnWidth = elWidth - result.totalWidth;
+          if (columnWidth > 0) {
+            remainWidth = columnWidth - Math.floor(columnWidth / result.emptyCount);
+            columnWidth = Math.floor(columnWidth / result.emptyCount);
+          } else {
+            columnWidth = this.columnWidth;
+          }
+
+          columnWidth = columnWidth < 40 ? 40 : columnWidth;
+          this.columnWidth = columnWidth;
+        }
+
+        this.orderedColumns.map((column) => {
+          const item = column;
+          if (!item.width && !item.hide) {
+            item.width = columnWidth;
+          }
+          return item;
+        });
+
+        if (remainWidth) {
+          this.orderedColumns[this.orderedColumns.length - 1].width += remainWidth;
+        }
       },
       setSort() {
         const index = this.getColumnIndex(this.sortField);
@@ -204,14 +286,16 @@
         const sortFn = this.sortOrder === 'desc' ? desc : asc;
 
         if (type === 'string') {
-            this.originStore.sort((a, b) => sortFn(a[index].toLowerCase(), b[index].toLowerCase()));
+          this.originStore.sort((a, b) => sortFn(a[ROW_DATA_INDEX][index].toLowerCase(),
+            b[ROW_DATA_INDEX][index].toLowerCase()));
         } else {
-            this.originStore.sort((a, b) => sortFn(a[index], b[index]));
+          this.originStore.sort((a, b) => sortFn(a[ROW_DATA_INDEX][index],
+            b[ROW_DATA_INDEX][index]));
         }
       },
       likeSearch(search, origin) {
         if (typeof search !== 'string' || origin === null) {
-            return false;
+          return false;
         }
 
         let regx = search.replace(new RegExp('([\\.\\\\\\+\\*\\?\\[\\^\\]\\$\\(\\)\\{\\}\\=\\!\\<\\>\\|\\:\\-])', 'g'), '\\$1');
@@ -226,13 +310,13 @@
         let result;
 
         if (type === 'equal') {
-            result = item[index] === value;
+          result = item[ROW_DATA_INDEX][index] === value;
         } else if (type === 'notEqual') {
-            result = item[index] !== value;
+          result = item[ROW_DATA_INDEX][index] !== value;
         } else if (type === 'like') {
-            result = this.likeSearch(`%${value}%`, item[index]);
+          result = this.likeSearch(`%${value}%`, item[ROW_DATA_INDEX][index]);
         } else if (type === 'notLike') {
-            result = !this.likeSearch(`%${value}%`, item[index]);
+          result = !this.likeSearch(`%${value}%`, item[ROW_DATA_INDEX][index]);
         }
 
         return result;
@@ -244,11 +328,11 @@
         let result;
 
         if (type === 'equal') {
-            result = item[index] === value;
+          result = item[ROW_DATA_INDEX][index] === value;
         } else if (type === 'greaterThan') {
-            result = item[index] > value;
+          result = item[ROW_DATA_INDEX][index] > value;
         } else if (type === 'lessThan') {
-            result = item[index] < value;
+          result = item[ROW_DATA_INDEX][index] < value;
         }
 
         return result;
@@ -258,9 +342,9 @@
         const filteredData = [];
 
         for (let ix = 0; ix < data.length; ix++) {
-            if (filterFn(data[ix], condition)) {
-                filteredData.push(data[ix]);
-            }
+          if (filterFn(data[ix], condition)) {
+              filteredData.push(data[ix]);
+          }
         }
 
         return filteredData;
@@ -271,8 +355,8 @@
         let index;
         let filter;
         let columnType;
-        const fields = Object.keys(this.filterList);
-        const store = _.cloneDeep(this.originStore);
+        const fields = Object.keys(this.filterList || {});
+        const store = this.originStore;
 
         for (let ix = 0; ix < fields.length; ix++) {
           field = fields[ix];
@@ -283,20 +367,20 @@
             for (let jx = 0; jx < filter.conditions.length; jx++) {
               if (filter.conditions[jx].use) {
                 if (!filteredStore.length) {
-                    filteredStore = this.getFilteredData(store, columnType, {
-                        ...filter.conditions[jx],
-                        index,
-                    });
+                  filteredStore = this.getFilteredData(store, columnType, {
+                      ...filter.conditions[jx],
+                      index,
+                  });
                 } else if (filter.method === 'or') {
-                    filteredStore.push(...this.getFilteredData(store, columnType, {
-                        ...filter.conditions[jx],
-                        index,
-                    }));
+                  filteredStore.push(...this.getFilteredData(store, columnType, {
+                      ...filter.conditions[jx],
+                      index,
+                  }));
                 } else {
-                    filteredStore = this.getFilteredData(filteredStore, columnType, {
-                        ...filter.conditions[jx],
-                        index,
-                    });
+                  filteredStore = this.getFilteredData(filteredStore, columnType, {
+                      ...filter.conditions[jx],
+                      index,
+                  });
                 }
               }
             }
@@ -305,8 +389,18 @@
 
         this.filteredStore = _.uniqBy(filteredStore, JSON.stringify);
       },
-      setStore(value) {
-        this.originStore = _.cloneDeep(value);
+      setStore(value, makeIndex = true) {
+        const store = [];
+        let checked;
+
+        if (makeIndex) {
+          for (let ix = 0; ix < value.length; ix++) {
+            checked = this.checked.findIndex(item => item === value[ix]) !== -1;
+            store.push([ix, checked, value[ix]]);
+          }
+        }
+
+        this.originStore = store;
 
         if (this.sortField) {
           this.setSort();
@@ -317,38 +411,6 @@
         }
 
         this.updateVScroll();
-
-        Object.freeze(this.originStore);
-      },
-      onSort(field) {
-        if (this.sortField === field) {
-          this.sortOrder = this.sortOrder === 'desc' ? 'asc' : 'desc';
-        } else {
-          this.sortField = field;
-          this.sortOrder = 'desc';
-        }
-
-        this.setStore(this.originStore);
-      },
-      onScroll() {
-        const el = this.$refs.body;
-        const scrollTop = el.scrollTop;
-        const scrollLeft = el.scrollLeft;
-        const lastTop = this.lastScroll.top;
-        const lastLeft = this.lastScroll.left;
-        const isHorizontal = !(scrollLeft === lastLeft);
-        const isVertical = !(scrollTop === lastTop);
-
-        if (isVertical) {
-          this.updateVScroll();
-        }
-
-        if (isHorizontal) {
-          this.updateHScroll();
-        }
-
-        this.lastScroll.top = scrollTop;
-        this.lastScroll.left = scrollLeft;
       },
       updateHScroll() {
         const headerEl = this.$refs.header;
@@ -374,6 +436,43 @@
         this.vScrollTopHeight = firstIndex * rowHeight;
         this.vScrollBottomHeight = totalScrollHeight - (this.viewStore.length * rowHeight)
           - this.vScrollTopHeight;
+      },
+      unCheckedRow(row) {
+        const index = this.originStore.findIndex(item => item === row);
+
+        if (index !== -1) {
+          this.$set(this.originStore, index, row);
+        }
+      },
+      onSort(field) {
+        if (this.sortField === field) {
+          this.sortOrder = this.sortOrder === 'desc' ? 'asc' : 'desc';
+        } else {
+          this.sortField = field;
+          this.sortOrder = 'desc';
+        }
+
+        this.setStore(this.originStore, false);
+      },
+      onScroll() {
+        const el = this.$refs.body;
+        const scrollTop = el.scrollTop;
+        const scrollLeft = el.scrollLeft;
+        const lastTop = this.lastScroll.top;
+        const lastLeft = this.lastScroll.left;
+        const isHorizontal = !(scrollLeft === lastLeft);
+        const isVertical = !(scrollTop === lastTop);
+
+        if (isVertical) {
+          this.updateVScroll();
+        }
+
+        if (isHorizontal) {
+          this.updateHScroll();
+        }
+
+        this.lastScroll.top = scrollTop;
+        this.lastScroll.left = scrollLeft;
       },
       onClickOption(e, field, index) {
         if (this.showColumnOption) {
@@ -402,6 +501,80 @@
 
         this.filterCondition = condition;
         this.showColumnOption = true;
+      },
+      onRowClick(event, row) {
+        if (!this.useSelect) {
+          return;
+        }
+
+        const cellInfo = event.target.dataset;
+        const rowData = row[ROW_DATA_INDEX];
+
+        this.$emit('update:selected', rowData);
+        this.$emit('click-row', event, row[ROW_INDEX], cellInfo.name, cellInfo.index, rowData);
+      },
+      onCheck(event, row) {
+        if (this.useCheckbox.mode === 'single' && this.prevCheckedRow.length) {
+          this.prevCheckedRow[1] = false;
+          this.unCheckedRow(this.prevCheckedRow);
+        }
+
+        if (row[ROW_CHECK_INDEX]) {
+          if (this.useCheckbox.mode === 'single') {
+            this.checkedRows = [row[ROW_DATA_INDEX]];
+          } else {
+            this.checkedRows.push(row[ROW_DATA_INDEX]);
+          }
+
+          if (this.checkedRows.length === this.originStore.length) {
+            this.isHeaderChecked = true;
+          }
+        } else {
+          if (this.isHeaderChecked) {
+            this.isHeaderChecked = false;
+          }
+
+          if (this.useCheckbox.mode === 'single') {
+            this.checkedRows = [];
+          } else {
+            this.checkedRows.splice(this.checkedRows.indexOf(row[ROW_DATA_INDEX]), 1);
+          }
+        }
+
+        this.prevCheckedRow = row;
+        this.$emit('update:checked', this.checkedRows);
+        this.$emit('check-one', event, row[ROW_INDEX], row[ROW_DATA_INDEX]);
+      },
+      onCheckAll(event) {
+        const status = this.isHeaderChecked;
+        const checked = [];
+        let item;
+
+        for (let ix = 0; ix < this.originStore.length; ix++) {
+          item = this.originStore[ix];
+          if (status) {
+            checked.push(item);
+          }
+
+          item[ROW_CHECK_INDEX] = status;
+        }
+
+        this.checkedRows = checked;
+        this.$emit('update:checked', checked);
+        this.$emit('check-all', event, checked);
+        this.$forceUpdate();
+      },
+      onResize() {
+        this.orderedColumns.map((column) => {
+          const item = column;
+
+          item.width = 0;
+
+          return item;
+        });
+
+        this.calculatedColumn();
+        this.$forceUpdate();
       },
     },
   };
@@ -468,13 +641,16 @@
   }
   .table-body table {
     clear: both;
-    margin: 0 auto;
+    /*margin: 0 auto;*/
     border-spacing: 0;
     border-collapse: collapse;
   }
   .table-body tr {
     white-space: nowrap;
     border-bottom: 1px solid black;
+  }
+  .table-body tr.selected {
+    background: #2D89EF;
   }
   .table-body td {
     display: inline-block;
