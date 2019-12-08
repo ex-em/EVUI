2b26687758e72450856f5735a09b75c9a0f3a92e
jykim <jykim@ex-em.com>
2019-12-09 08:24:58 +0900

    [#449] Fix style in menu (#463)
    1. 테마별 스타일 처리
    2. 깊이별 스타일 처리

--- src/components/menu/menu.nav.vue
+++ src/components/menu/menu.nav.vue
@@ -1,11 +1,12 @@
 <template>
   <ul
     :style="`width: ${width}`"
+    class="ev-menu"
   >
     <li
       v-for="menuItem in menu"
       :key="menuItem.name"
-      class="ev-menu-sub"
+      class="ev-menu-sub split"
     >
       <menu-item
         v-bind="menuItem"
@@ -46,12 +47,55 @@
   };
 </script>
 <style lang="scss">
-  .ev-menu-sub,
-  .ev-menu-item {
+  @import '~@/styles/default';
+
+  .ev-menu {
+    @include evThemify() {
+      color: evThemed('menu-color');
+    }
+  }
+
+  .ev-menu-sub {
     list-style-type: none;
+
+    @include evThemify() {
+      color: evThemed('menu-color');
+    }
+
+    &.split:not(:last-child) {
+      @include evThemify() {
+        border-bottom: 1px solid evThemed('menu-sub-split');
+      }
+    }
+
+    &.split:not(:first-child) {
+      @include evThemify() {
+        padding-top: 18px;
+      }
+    }
+
+    & > .first {
+      padding: 0 18px;
+    }
   }
 
   .ev-menu-item {
-    padding: 8px 18px;
+    display: flex;
+    align-items: center;
+    user-select: none;
+    cursor: pointer;
+    list-style-type: none;
+    padding-bottom: 18px;
+    font-size: 13px;
+
+    &.first {
+      font-size: 16px;
+    }
+
+    &.selected {
+      @include evThemify() {
+        color: evThemed('menu-selected-bg');
+      }
+    }
   }
 </style>
