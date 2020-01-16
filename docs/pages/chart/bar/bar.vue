71a5367f176343acb69000085aad0165e144ecf3
jwlee <jwlee@ex-em.com>
2020-01-16 16:50:45 +0900

    [#478] Chart Item Select API 개발 ######## - label 기반 item select 처리 - 차트 옵션 기본값 수정

--- docs/pages/chart/bar/bar.vue
+++ docs/pages/chart/bar/bar.vue
@@ -20,42 +20,16 @@
 </template>
 
 <script>
-  import Default from './examples/default';
-  import Stack from './examples/stack';
-  import Horizontal from './examples/horizontal';
   import Time from './examples/time';
-  import Range from './examples/range';
 
   export default {
     data() {
       return {
         title: 'Bar Chart',
         components: [{
-          title: 'Default',
-          component: Default,
-          url: './docs/pages/chart/bar/examples/default.vue',
-          description: `Bar Chart는 Category 기반의 데이터를 표현하는 차트로, 각 Category에 대한 Bar의 높이가 나타내는 값에 비례합니다.
-                        계열이 여러개가 존재할 경우 동일한 넓이의 Bar를 생성하여 Category 기준으로 각 계열을 표현합니다.`,
-        }, {
-          title: 'Stack',
-          component: Stack,
-          url: './docs/pages/chart/bar/examples/stack.vue',
-          description: 'Category 기반의 막대 데이터를 누적하여 표현합니다.',
-        }, {
-          title: 'Horizontal',
-          component: Horizontal,
-          url: './docs/pages/chart/bar/examples/horizontal.vue',
-          description: `Horizontal Bar Chart는 각 Category에 대한 Bar의 널이가 나타내는 값에 비례합니다.
-                        계열이 여러개가 존재할 경우 동일한 높이의 Bar를 생성하여 Category 기준으로 각 계열을 표현합니다.`,
-        }, {
           title: 'Time',
           component: Time,
           url: './docs/pages/chart/bar/examples/time.vue',
-          description: 'RealTime 처리를 위한 차트입니다.',
-        }, {
-          title: 'Range',
-          component: Range,
-          url: './docs/pages/chart/bar/examples/range.vue',
           description: 'RealTime 처리를 위한 차트입니다.',
         }],
       };
--- docs/pages/chart/bar/examples/time.vue
+++ docs/pages/chart/bar/examples/time.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
     <ev-chart
+      ref="evChart"
       :data="getChartData"
       :options="chartOptions"
+      @select-item="onSelectItem"
     />
     <div style="position: absolute; top: 0; right: 0;">
       <ev-button
@@ -20,16 +22,24 @@
     data() {
       return {
         series: {
-          series1: { name: 'series#1', show: true, type: 'bar' },
-          series2: { name: 'series#2', show: true, type: 'bar' },
-          series3: { name: 'series#3', show: true, type: 'bar' },
-          series4: { name: 'series#4', show: true, type: 'bar' },
+          series1: { name: 'warning', show: true, type: 'bar' },
+          series2: { name: 'critical', show: true, type: 'bar' },
         },
         groups: [
-          ['series1', 'series2', 'series3', 'series4'],
+          ['series1', 'series2'],
         ],
-        labels: [],
-        chartData: {},
+        labels: [
+          +new Date('2020-01-14 00:00:00'),
+          +new Date('2020-01-14 01:00:00'),
+          +new Date('2020-01-14 02:00:00'),
+          +new Date('2020-01-14 03:00:00'),
+          +new Date('2020-01-14 04:00:00'),
+          +new Date('2020-01-14 05:00:00'),
+        ],
+        chartData: {
+          series1: [null, 20, null, 0, 30, 50], // eslint-disable-line
+          series2: [10, null, null, 0, 30, 50], // eslint-disable-line
+        },
         chartOptions: {
           width: '100%',
           height: '100%',
@@ -47,8 +57,8 @@
             type: 'time',
             showGrid: true,
             categoryMode: true,
-            timeFormat: 'HH:mm:ss',
-            interval: 3000,
+            timeFormat: 'HH:mm',
+            interval: 'hour',
           }],
           axesY: [{
             type: 'linear',
@@ -78,14 +88,27 @@
       },
     },
     created() {
-      this.makeInitData();
+      // this.makeInitData();
     },
     destroyed() {
       if (this.liveInterval) {
         clearTimeout(this.liveInterval);
       }
     },
+    mounted() {
+      setTimeout(() => {
+        this.select();
+      }, 5000);
+    },
     methods: {
+      select() {
+        console.log(this.$refs.evChart.selectItemByLabel(+new Date('2020-01-14 08:00:00')));
+        console.log(this.$refs.evChart.selectItemByLabel(+new Date('2020-01-14 04:00:00')));
+        console.log(this.$refs.evChart.selectItemByLabel(+new Date('2020-01-14 08:00:00')));
+      },
+      onSelectItem(item) {
+        console.log(item, 'selecttt');
+      },
       onClickLiveBtn(event) {
         this.event = event;
         this.liveBtnInfo.text = this.liveBtnInfo.text === 'Live' ? 'Stop' : 'Live';
