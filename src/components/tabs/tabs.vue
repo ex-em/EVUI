267e53f39c90e7dc10cf6be4a04bff2851427301
Min Ji, Choi <30827184+mmindy@users.noreply.github.com>
2020-01-16 09:41:58 +0900

    [#499] tab 색상 및 디자인 수정 (#500)
    * [#499] tab 스타일 변경
    ######
    시안 반영
    
    * [#499] tab 스타일 변경
    ######
    시안 반영
    
    * [#499] tab 스타일 변경
    ######
    tab-body 테마 색상 추가(active tab 배경색과 동일)

--- src/components/tabs/tabs.vue
+++ src/components/tabs/tabs.vue
@@ -366,6 +366,7 @@
     @include evThemify() {
       color: evThemed('font-color-base');
       border: $border-solid evThemed('tab-border');
+      background-color: evThemed('tab-active-bg');
       border-top: 0;
     }
   }
@@ -399,7 +400,6 @@
     border-right: none;
   }
   .ev-tabs-item {
-    margin-right: 4px;
     height: 30px;
     box-sizing: border-box;
     display: inline-block;
@@ -409,16 +409,19 @@
     user-select: none;
 
     @include evThemify() {
-      color: evThemed('font-color-base');
+      color: evThemed('tab-color');
       border: $border-solid evThemed('tab-border');
       background-color: evThemed('tab-bg');
     }
   }
   .ev-tabs-item.active {
     transform: translateZ(0);
-    color: $color-primary;
     border-bottom: none;
-    background-color: $color-white;
+
+    @include evThemify() {
+      color: evThemed('tab-active-color');
+      background-color: evThemed('tab-active-bg');
+    }
   }
   .ev-tabs-item.dragover {
     border-left: 3px solid blue;
