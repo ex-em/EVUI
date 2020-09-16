183e85405bb3235c75c3c3751a87f1e21945d079
kimdoeun <kdeun1@ex-em.com>
2020-09-16 18:03:16 +0900

    [#630] Checkbox, CheckboxGroup 컴포넌트 개발 ################ - Checkbox 컴포넌트 개발 - CheckboxGroup 컴포넌트 개발 - 각 컴포넌트의 샘플 코드 작성

---
+++ docs/views/checkbox/example/CheckboxGroup.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <h5># Checkbox Group</h5>
+    <br>
+    <EvCheckboxGroup
+      v-model="checkboxGroup"
+    >
+      <EvCheckbox label="Option A">A</EvCheckbox>
+      <EvCheckbox label="Option B" />
+      <EvCheckbox label="Option C" />
+      <EvCheckbox label="Option D" />
+    </EvCheckboxGroup>
+    <br>
+    <br>
+    checkboxGroup : {{ checkboxGroup }}
+    <br>
+    <br>
+    <h5># Checkbox Group Event</h5>
+    <br>
+    <EvCheckboxGroup
+      v-model="checkboxGroup2"
+    >
+      <EvCheckbox label="Option A">A</EvCheckbox>
+      <EvCheckbox label="Option B" />
+      <EvCheckbox label="Option C" />
+      <EvCheckbox label="Option D" />
+    </EvCheckboxGroup>
+    <br>
+    <button @click="clickButton">
+      Add 'Option A'
+    </button>
+    <br>
+    checkboxGroup2 : {{ checkboxGroup2 }}
+    <br>
+    <br>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  components: {},
+  setup() {
+    const checkboxGroup = ref(['Option A', 'Option B']);
+    const checkboxGroup2 = ref([]);
+
+    const clickButton = () => {
+      if (!checkboxGroup2.value.length) {
+        checkboxGroup2.value.push('Option A');
+      }
+    };
+
+    return {
+      checkboxGroup,
+      checkboxGroup2,
+      clickButton,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
--- docs/views/checkbox/index.vue
+++ docs/views/checkbox/index.vue
@@ -21,7 +21,9 @@ import { defineAsyncComponent } from 'vue';
 import { parseComponent } from 'vue-template-compiler';
 import Example from '../../components/Example';
 import Default from './example/Default';
+import CheckboxGroup from './example/CheckboxGroup';
 import DefaultRaw from '!!raw-loader!./example/Default';
+import CheckboxGroupRaw from '!!raw-loader!./example/CheckboxGroup';
 import MarkdownView from '../../components/MarkdownView';
 import CheckboxMd from '!!raw-loader!./api/checkbox.md';
 
@@ -34,11 +36,6 @@ export default {
   inheritAttrs: false,
   setup() {
     const mdText = CheckboxMd;
-    const { template, script } = parseComponent(DefaultRaw);
-    const codeTextObj = {
-      template: template.content,
-      script: script.content,
-    };
 
     const components = [
       {
@@ -46,7 +43,20 @@ export default {
         description: '여러 개의 선택 사항을 고르기 위한 단일 체크 박스의 기능입니다.',
         component: defineAsyncComponent(() => Promise.resolve(Default)),
         url: './docs/views/checkbox/example/Default.vue',
-        codeText: codeTextObj,
+        codeText: {
+          template: parseComponent(DefaultRaw)?.template?.content,
+          script: parseComponent(DefaultRaw)?.script?.content,
+        },
+      },
+      {
+        title: 'CheckboxGroup',
+        description: '체크박스 그룹 기능입니다..',
+        component: defineAsyncComponent(() => Promise.resolve(CheckboxGroup)),
+        url: './docs/views/checkbox/example/CheckboxGroup.vue',
+        codeText: {
+          template: parseComponent(CheckboxGroupRaw)?.template?.content,
+          script: parseComponent(CheckboxGroupRaw)?.script?.content,
+        },
       },
     ];
 
