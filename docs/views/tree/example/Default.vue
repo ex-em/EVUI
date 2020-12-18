0620c8ac8c6213b64fb63c74584189cc9da9e8f1
BoKyeongShin <49548560+BoKyeongShin@users.noreply.github.com>
2020-12-18 09:28:06 +0900

    [#734][3.0] Tree 컴포넌트 마이그레이션 진행 (#735)
    * [#734][3.0] Tree 컴포넌트 마이그레이션 진행
    ###########
    - router> index.js: tree 메뉴 로드 정보 수정
    - views> tree에 tree.md, Default.vue, props.js 파일 추가하고 index.vue 파일은 제거
    - src> components> tree에 index.js(ev-tree 등록), tree.vue, treenode.vue 파일 추가
    - main.js: 등록한 ev-tree 컴포넌트 전역에서 사용할 수 있도록 추가
    - Tree.vue: 트리 정보를 재정의하고 체크박스 선택 해제 시마다 현재 체크된 노드를 기준으로 체크박스 설정을 수정하는 로직이 구현
    - TreeNode.vue: 가시적인 트리 구조가 구현되어 있고 체크박스 선택, 컨텐츠 클릭, 트리 펼침/닫힘 처리가 수행됨
    
    * [#734][3.0] Tree.vue props default 선언 방식 수정
    ###########
    - 다른 props의 default 선언 방식과의 통일성을 위해 수정
    
    * [#734][3.0] Tree 컴포넌트 이벤트 동작 안하는 문제로 package-lock 파일 다시 추가
    ###########
    - Tree 컴포넌트 이벤트 동작 안하는 문제로 package-lock 파일 다시 추가
    
    * [#734][3.0] 1차 피드백 반영
    ###########
    - 이벤트 명 수정(click-node, dbclick-node, check로 수정)
    - 각 속성의 특징을 볼 수 있는 예제 추가
    - Default.vue에서 함수사용을 화살표 함수로 수정
    - 기본 expand/collapse 아이콘 변경, expand 시 rotate처리하여 보여지도록 수정, 열고 닫을 때 애니메이션 추가
    - 처음에 체크박스 선택될 때 rebuildTree가 수행되지 않아 부모 체크박스가 선택되지 않는 문제 수정
    - md 기술 문서 수정
    - disable-eslint 해놨던 제거 하고 불가피하게 린트 사용 막을 한 줄만 'eslint-disable-line no-param-reassign' 추가
    - 아이콘 간격 수정
    
    * [#734][3.0] 2차 피드백 반영
    ###########
    - value 속성 추가하여 value가 없을 경우 title + nodekey를 가지도록 추가함 (nodeKey를 추가한 이유는 동일한 title이 있을 수 있기 때문에)
    - value를 추가하면서 click, dbclick, check 이벤트 발생 시 선택되거나 체크된 노드 정보 모두 emit하던 방식에서 {title, value}를 emit하는 방식으로 수정

---
+++ docs/views/tree/example/Default.vue
@@ -0,0 +1,292 @@
+<template>
+  <div class="case">
+    <p class="case-title">Common</p>
+    <ev-tree
+      :data="commonData"
+      @click-node="getClickedNode"
+    />
+    <div class="description">
+      <div class="badge yellow">
+        클릭된 노드
+      </div>
+      {{ clickedNodeInfo }}
+    </div>
+  </div>
+  <div class="case">
+    <p class="case-title">Checkbox</p>
+    <ev-tree
+      :data="checkboxExampleData"
+      :use-checkbox="true"
+      @check="getCheckedNode"
+    />
+    <div class="description">
+      <div class="badge yellow">
+        체크박스 선택된 노드들
+      </div>
+      {{ checkedNodeInfo }}
+    </div>
+  </div>
+  <div class="case">
+    <p class="case-title">Dblclick-node</p>
+    <ev-tree
+      :data="commonData"
+      @dblclick-node="getDblClickedNode"
+    />
+    <div class="description">
+      <div class="badge yellow">
+        더블 클릭된 노드
+      </div>
+      {{ dbclickedNodeInfo }}
+    </div>
+  </div>
+  <div class="case">
+    <p class="case-title">Disabled</p>
+    <ev-tree :data="disableExData"/>
+    <div class="description">
+      'disabled' 속성 추가 시 클릭이 불가 합니다.
+    </div>
+  </div>
+  <div class="case">
+    <p class="case-title">ContextMenu</p>
+    <ev-tree
+      :data="contextmenuExData"
+      :context-menu-items="menuItems"
+    />
+    <div class="description">
+      <div class="badge yellow">
+        클릭한 컨텍스메뉴명
+      </div>
+      {{ contextMenuInfo }}
+    </div>
+  </div>
+  <div class="case">
+    <p class="case-title">Custom Expand/Collapse Icon</p>
+    <ev-tree
+      :data="commonData"
+      expand-icon="ev-icon-square-plus"
+      collapse-icon="ev-icon-square-minus"
+    />
+    <div class="description">
+      'expand-icon'과 'collapse-icon' 속성을 통해 아이콘을 변경할 수 있습니다.
+    </div>
+  </div>
+  <div class="case">
+    <p class="case-title">IconClass</p>
+    <ev-tree :data="iconClassExData"/>
+    <div class="description">
+      'iconClass'속성을 사용하면 트리 노드 내부에 아이콘을 추가 수 있습니다.
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const commonData = ref([
+      {
+        title: 'Root',
+        value: 'root',
+        expand: true,
+        children: [
+          {
+            title: 'Parent A',
+            value: 'p1',
+            expand: true,
+            children: [
+              {
+                title: 'Leaf AA',
+                value: 'p1a',
+                expand: true,
+                children: [
+                  {
+                    title: 'Leaf AAA',
+                  },
+                ],
+              },
+              {
+                title: 'Leaf AB',
+                value: 'p1b',
+              },
+            ],
+          },
+        ],
+      },
+    ]);
+    const checkboxExampleData = ref([
+      {
+        title: 'Root',
+        expand: true,
+        children: [
+          {
+            title: 'Parent A',
+            expand: true,
+            children: [
+              {
+                title: 'Child AA',
+                expand: true,
+                children: [
+                  {
+                    title: 'GrandChild A',
+                    checked: true,
+                  },
+                ],
+              },
+              {
+                title: 'Child AB',
+              },
+            ],
+          },
+          {
+            title: 'Parent B',
+            expand: true,
+            children: [
+              {
+                title: 'Child BA',
+              },
+              {
+                title: 'Child BB',
+                checked: true,
+              },
+            ],
+          },
+        ],
+      },
+    ]);
+    const disableExData = ref([
+      {
+        title: 'Root',
+        expand: true,
+        children: [
+          {
+            title: 'Parent A',
+            expand: true,
+            disabled: true,
+          },
+          {
+            title: 'Parent B',
+            expand: true,
+            children: [
+              {
+                title: 'Leaf BA',
+                disabled: true,
+              },
+              {
+                title: 'Leaf BB',
+              },
+            ],
+          },
+        ],
+      },
+    ]);
+    const contextmenuExData = ref([
+      {
+        title: 'Root',
+        expand: true,
+        children: [
+          {
+            title: 'Parent A',
+            expand: true,
+          },
+          {
+            title: 'Parent B',
+            expand: true,
+            children: [
+              {
+                title: 'Leaf BA',
+              },
+              {
+                title: 'Leaf BB',
+              },
+            ],
+          },
+        ],
+      },
+    ]);
+    const iconClassExData = ref([
+      {
+        title: 'Root',
+        expand: true,
+        children: [
+          {
+            title: 'Parent A',
+            expand: true,
+            iconClass: 'ev-icon-folder',
+            children: [
+              {
+                title: 'Leaf AA',
+                expand: true,
+                iconClass: 'ev-icon-document-vertically',
+              },
+              {
+                title: 'Leaf AB',
+              },
+            ],
+          },
+          {
+            title: 'Parent B',
+            expand: true,
+            iconClass: 'ev-icon-folder2',
+            children: [
+              {
+                title: 'Leaf BA',
+              },
+              {
+                title: 'Leaf BB',
+              },
+            ],
+          },
+        ],
+      },
+    ]);
+
+    const clickedNodeInfo = ref({});
+    const checkedNodeInfo = ref({});
+    const dbclickedNodeInfo = ref({});
+    const contextMenuInfo = ref('');
+
+    const menuItems = ref([
+      {
+        text: 'ContextMenu1',
+        click: () => { contextMenuInfo.value = 'ContextMenu1'; },
+      },
+      {
+        text: 'ContextMenu2',
+        click: () => { contextMenuInfo.value = 'ContextMenu2'; },
+      },
+    ]);
+
+    const getCheckedNode = (checkedNode) => {
+      checkedNodeInfo.value = checkedNode;
+    };
+
+    const getClickedNode = (clickedNode) => {
+      clickedNodeInfo.value = clickedNode;
+    };
+
+    const getDblClickedNode = (clickedNode) => {
+      dbclickedNodeInfo.value = clickedNode;
+    };
+
+    return {
+      commonData,
+      checkboxExampleData,
+      disableExData,
+      contextmenuExData,
+      iconClassExData,
+      menuItems,
+      clickedNodeInfo,
+      dbclickedNodeInfo,
+      checkedNodeInfo,
+      contextMenuInfo,
+      getCheckedNode,
+      getClickedNode,
+      getDblClickedNode,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/components/tree/Tree.vue
@@ -0,0 +1,238 @@
+<template>
+  <div class="ev-tree-view">
+    <tree-node
+      v-for="(item, i) in treeNodeData"
+      :key="i"
+      :data="item"
+      :use-checkbox="useCheckbox"
+      :expand-icon="expandIcon"
+      :collapse-icon="collapseIcon"
+      @update-checked-info="updateCheckedInfo"
+      @click-node="clickContent"
+      @dblclick-node="dblClickContent"
+      @show-context-menu="getContextMenuFlag"
+      @contextmenu.prevent="showContextMenu"
+    />
+    <div v-if="!treeNodeData.length">No data</div>
+    <ev-context-menu
+      v-if="contextMenuItems.length"
+      ref="contextMenu"
+      :items="contextMenuItems"
+    />
+  </div>
+</template>
+
+<script>
+import { ref, reactive, watch, onMounted } from 'vue';
+import TreeNode from './TreeNode';
+
+export default {
+  name: 'EvTree',
+  components: { TreeNode },
+  props: {
+    data: {
+      type: Array,
+      default: () => [],
+    },
+    useCheckbox: {
+      type: Boolean,
+      default: false,
+    },
+    emptyText: {
+      type: String,
+      default: 'No Data',
+    },
+    expandIcon: {
+      type: String,
+      default: '',
+    },
+    collapseIcon: {
+      type: String,
+      default: '',
+    },
+    contextMenuItems: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  emits: {
+    'click-node': null,
+    'dblclick-node': null,
+    check: Array,
+  },
+  setup(props, { emit }) {
+    let treeNodeData = reactive(props.data);
+    let allNodeInfo = [];
+    const contextMenu = ref(null);
+    let contextMenuFlag = false; // flag for showing contextMenu or not
+
+    function updateTreeUp(nodeKey) {
+      const parentKey = allNodeInfo[nodeKey].parent;
+      if (typeof parentKey === 'undefined') {
+        return;
+      }
+      const node = allNodeInfo[nodeKey].node;
+      const parent = allNodeInfo[parentKey].node;
+      if (node.checked === parent.checked && node.indeterminate === parent.indeterminate) {
+        return; // no need to update upwards
+      }
+
+      if (node.checked) {
+        parent.checked = parent.children.every(n => n.checked);
+        parent.indeterminate = !parent.checked;
+      } else {
+        const fn = n => n.checked || node.indeterminate;
+        parent.checked = false;
+        parent.indeterminate = parent.children.some(fn);
+      }
+      updateTreeUp(parentKey);
+    }
+
+    function updateTreeDown(node, changes = {}) {
+      const keys = Object.keys(changes);
+      for (let ix = 0; ix < keys.length; ix++) {
+        node[keys[ix]] = changes[keys[ix]]; // eslint-disable-line no-param-reassign
+      }
+
+      if (node.children) {
+        node.children.forEach((child) => {
+          updateTreeDown(child, changes);
+        });
+      }
+    }
+
+    function getAllNodeInfo() { // return the array to easily search parents and children
+      let keyCounter = 0;
+      const flatTree = [];
+      const valueArr = [];
+
+      function flattenChildren(n, parent) {
+        const node = n;
+        node.nodeKey = keyCounter++;
+
+        // add 'selected' property for highlighting clicked node
+        if (!('selected' in node)) {
+          node.selected = false;
+        }
+
+        // check 'value' property and add nodeKey if same value already exists
+        if ('value' in node && valueArr.includes(node.value)) {
+          console.warn('The \'value\' of data should be unique.');
+          node.value += node.nodeKey;
+        } else if (!('value' in node)) {
+          node.value = node.title + node.nodeKey;
+        }
+        valueArr.push(node.value);
+
+        flatTree[node.nodeKey] = { node, nodeKey: node.nodeKey };
+        if (typeof parent !== 'undefined') {
+          flatTree[node.nodeKey].parent = parent.nodeKey;
+          flatTree[parent.nodeKey].children.push(node.nodeKey);
+        }
+        if (node.children) {
+          flatTree[node.nodeKey].children = [];
+          node.children.forEach(child => flattenChildren(child, node));
+        }
+      }
+
+      flattenChildren(treeNodeData[0]);
+      return flatTree;
+    }
+
+    allNodeInfo = getAllNodeInfo();
+
+    function getCheckedNodes() {
+      return allNodeInfo.filter(obj => obj.node.checked).map(obj => obj.node);
+    }
+
+    function rebuildTree() { // rebuild the tree through checked nodes
+      const checkedNodes = getCheckedNodes();
+      checkedNodes.forEach((node) => {
+        updateTreeDown(node, { checked: true });
+        // propagate upwards
+        const parentKey = allNodeInfo[node.nodeKey].parent;
+        if (!parentKey && parentKey !== 0) {
+          return;
+        }
+        const parent = allNodeInfo[parentKey].node;
+        const childHasCheckSetter = typeof node.checked !== 'undefined' && node.checked;
+        if (childHasCheckSetter && parent.checked !== node.checked) {
+          updateTreeUp(node.nodeKey); // update tree upwards
+        }
+      });
+    }
+
+    function updateCheckedInfo({ nodeKey, isChecked }) {
+      const node = allNodeInfo[nodeKey].node;
+      node.checked = isChecked;
+      node.indeterminate = false;
+      updateTreeUp(nodeKey); // propagate up
+      updateTreeDown(node, { checked: isChecked, indeterminate: false }); // reset `indeterminate`
+      const checkedNodes = allNodeInfo.filter(obj => obj.node.checked)
+        .map(obj => ({
+            title: obj.node.title,
+            value: obj.node.value,
+          }));
+      emit('check', checkedNodes);
+      rebuildTree();
+    }
+
+    function clickContent(nodeKey) {
+      const clickedNode = allNodeInfo[nodeKey].node;
+      // reset other selected node to false
+      for (let ix = 0; ix < allNodeInfo.length; ix++) {
+        if (allNodeInfo[ix].node.nodeKey !== nodeKey) {
+          allNodeInfo[ix].node.selected = false;
+        }
+      }
+      emit('click-node', { title: clickedNode.title, value: clickedNode.value });
+    }
+
+    function dblClickContent(nodeKey) {
+      const dbClickedContent = allNodeInfo[nodeKey].node;
+      emit('dblclick-node', { title: dbClickedContent.title, value: dbClickedContent.value });
+    }
+
+    const showContextMenu = (e) => {
+      if (props.contextMenuItems.length && contextMenuFlag) {
+        contextMenu.value.show(e);
+        contextMenuFlag = false; // reset contextmenuFlag
+      }
+    };
+
+    const getContextMenuFlag = (isShow, e) => {
+      contextMenuFlag = isShow;
+      showContextMenu(e);
+    };
+
+    watch(props.data, (newData) => {
+      treeNodeData = newData;
+      allNodeInfo = getAllNodeInfo();
+    });
+
+    onMounted(() => {
+      rebuildTree();
+      const checkedNodes = getCheckedNodes();
+      if (checkedNodes.length) {
+        emit('check', checkedNodes.map(node => ({
+          title: node.title,
+          value: node.value,
+        })));
+      }
+    });
+
+    return {
+      treeNodeData,
+      contextMenu,
+      updateCheckedInfo,
+      clickContent,
+      dblClickContent,
+      showContextMenu,
+      getContextMenuFlag,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
