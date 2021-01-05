c58c1a680ec0520b84bd5782e04ea7b670a0dc56
yell <yell@ex-em.com>
2021-01-05 11:39:01 +0900

    [#727][3.0] Grid 컴포넌트 개발 ####################### - example.spec.js 이전 상태로 변경

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,7 +1,7 @@
 import { shallowMount } from '@vue/test-utils';
 import Checkbox from '@/components/checkbox/Checkbox';
 
-describe('Grid.vue', () => {
+describe('Checkbox.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message';
     const wrapper = shallowMount(Checkbox, {
