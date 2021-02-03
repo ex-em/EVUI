843adfa9282bf088861049d74221ba3b9c4358e8
yell <yell@ex-em.com>
2021-02-03 16:26:09 +0900

    [#727][3.0] Grid 컴포넌트 개발 ####################### - 리사이즈 관련 'vue-resize-observer' 라이브러리 추가

--- src/components/grid/index.js
+++ src/components/grid/index.js
@@ -1,7 +1,9 @@
+import VueResizeObserver from 'vue-resize-observer';
 import EvGrid from './Grid';
 
 EvGrid.install = (app) => {
   app.component(EvGrid.name, EvGrid);
+  app.use(VueResizeObserver);
 };
 
 export default EvGrid;
