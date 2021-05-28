bd823f499289875012eac48ec36e2a6c03823a3e
yell <yell@ex-em.com>
2021-05-28 10:00:19 +0900

    [#815][3.0] Grid 기능 추가 ####################### - Toolbar, Count 컴포넌트 추가

---
+++ src/components/grid/grid.count.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="gridCount">
+    <slot name="countWrapper"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EvGridCount',
+};
+</script>
+
+<style lang="scss">
+@import '../../style/index.scss';
+</style>
