7311b1b86a382d4e395ed99079742ceef6f87cd6
kdeun1 <kdeun1@ex-em.com>
2021-08-04 20:42:18 +0900

    [#724][3.0] Tab 내부의 ctx 로직 삭제 (#854)
    ################
    - getCurrentInstance() 내 ctx 가져오는 로직을 지우고 mv의 값을 바로 provide로 넘김

--- src/components/tabPanel/TabPanel.vue
+++ src/components/tabPanel/TabPanel.vue
@@ -31,7 +31,7 @@ export default {
   },
   setup(props) {
     const evTabs = inject('evTabs', null);
-    const mv = computed(() => evTabs.ctx.mv);
+    const mv = computed(() => evTabs);
     const isSelected = computed(() => props.value === mv.value);
 
     return {
