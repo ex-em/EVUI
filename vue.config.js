606d34ab9ac295236376e94f57e4bc6f38f62eed
byeongwan <byeongwan@ex-em.com>
2022-06-08 15:38:11 +0900

    [#1205] Github Actions CI/CD 적용 - Docs #################### - PRODUCTION_PATH 환경변수 추가 및 적용

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const StyleLintPlugin = require('stylelint-webpack-plugin');
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/EVUI/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? process.env.VUE_APP_PRODUCTION_PATH : '/',
   outputDir: 'dist',
   devServer: {
     overlay: false,
