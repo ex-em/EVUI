30accc0a948123b9a8a5839d87dece5fa4fe122e
kdeun1 <kdeun1@users.noreply.github.com>
2022-06-09 02:43:23 +0000

    deploy: a76913ce54dc68ca3a505efe6f6e6fd2a75dab7f

---
+++ js/chunk-2d0d78a2.90967d56.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0d78a2"],{"76f3":function(n,c,e){"use strict";e.r(c);var t=e("7a23");function o(n,c,e,o,u,r){return Object(t["openBlock"])(),Object(t["createBlock"])("div",null," COMP2 ")}var u={setup:function(){}},r=e("6b0d"),a=e.n(r);const d=a()(u,[["render",o]]);c["default"]=d}}]);
+//# sourceMappingURL=chunk-2d0d78a2.90967d56.js.map
