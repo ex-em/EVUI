2ec1e4dcaf2d7a7825b127cce7a82c82ddd680a1
zoe <seoyeon3472@gmail.com>
2022-09-16 14:10:40 +0900

    [ 2.0 ] Grid > Filter On > filter 관련 에러

--- docs/pages/grid/examples/default.vue
+++ docs/pages/grid/examples/default.vue
@@ -99,167 +99,167 @@
   </div>
 </template>
 <script>
-const countries = [
-  'Russia', 'Canada', 'United States', 'China', 'Brazil',
-  'Australia', 'India', 'Argentina', 'Kazakhstan', 'Algeria',
-  'Denmark', 'Mexico', 'Indonesia', 'Sudan', 'Libya',
-  'Iran', 'Japan', 'Korea', 'Egypt', 'Ethiopia',
-];
-export default {
-  name: 'New',
-  data() {
-    return {
-      selected: [],
-      checked: [],
-      columns: [
-        { caption: 'ID', field: 'id', type: 'number', width: 50 },
-        { caption: 'Country', field: 'country', type: 'string' },
-        { caption: 'Area', field: 'area', type: 'number', hide: true },
-        { caption: 'Population', field: 'population', type: 'number' },
-        { caption: 'GDP', field: 'gdp', type: 'number' },
-        { caption: 'test1', field: 'test1', type: 'boolean', width: 50, render: { use: true } },
-        { caption: 'test2', field: 'test2', type: 'boolean', render: { use: true } },
-        {
-          caption: 'test3',
-          field: 'test3',
-          type: 'string',
-          width: 300,
-          render: {
-            use: true,
-            props: {
-              children: {
-                typeA: 'radio-01',
-                typeB: 'radio-02',
-                typeC: 'radio-03',
+  const countries = [
+    'Russia', 'Canada', 'United States', 'China', 'Brazil',
+    'Australia', 'India', 'Argentina', 'Kazakhstan', 'Algeria',
+    'Denmark', 'Mexico', 'Indonesia', 'Sudan', 'Libya',
+    'Iran', 'Japan', 'Korea', 'Egypt', 'Ethiopia',
+  ];
+  export default {
+    name: 'New',
+    data() {
+      return {
+        selected: [],
+        checked: [],
+        columns: [
+          { caption: 'ID', field: 'id', type: 'number', width: 50 },
+          { caption: 'Country', field: 'country', type: 'string' },
+          { caption: 'Area', field: 'area', type: 'number', hide: true },
+          { caption: 'Population', field: 'population', type: 'number' },
+          { caption: 'GDP', field: 'gdp', type: 'number' },
+          { caption: 'test1', field: 'test1', type: 'boolean', width: 50, render: { use: true } },
+          { caption: 'test2', field: 'test2', type: 'boolean', render: { use: true } },
+          {
+            caption: 'test3',
+            field: 'test3',
+            type: 'string',
+            width: 300,
+            render: {
+              use: true,
+              props: {
+                children: {
+                  typeA: 'radio-01',
+                  typeB: 'radio-02',
+                  typeC: 'radio-03',
+                },
               },
             },
           },
-        },
-        { caption: 'test4', field: 'test4', type: 'string', render: { use: true } },
-        { caption: 'test5', field: 'test5', type: 'string', render: { use: true } },
-        { caption: 'test6', field: 'test6', type: 'number', render: { use: true } },
-        {
-          caption: 'test7',
-          field: 'test7',
-          type: 'string',
-          width: 100,
-          render: {
-            use: true,
-            props: {
-              list: [
-                { name: 'normal', value: 'normal' },
-                { name: 'warning', value: 'warning' },
-                { name: 'critical', value: 'critical' },
-              ],
+          { caption: 'test4', field: 'test4', type: 'string', render: { use: true } },
+          { caption: 'test5', field: 'test5', type: 'string', render: { use: true } },
+          { caption: 'test6', field: 'test6', type: 'number', render: { use: true } },
+          {
+            caption: 'test7',
+            field: 'test7',
+            type: 'string',
+            width: 100,
+            render: {
+              use: true,
+              props: {
+                list: [
+                  { name: 'normal', value: 'normal' },
+                  { name: 'warning', value: 'warning' },
+                  { name: 'critical', value: 'critical' },
+                ],
+              },
             },
           },
-        },
-        {
-          caption: 'test8',
-          field: 'test8',
-          type: 'array',
-          width: 200,
-          render: {
-            use: true,
-            props: {
-              list: [
-                { name: 'normal', value: 'normal' },
-                { name: 'warning', value: 'warning' },
-                { name: 'critical', value: 'critical' },
-                { name: 'block', value: 'block' },
-                { name: 'major', value: 'major' },
-                { name: 'minor', value: 'minor' },
-              ],
+          {
+            caption: 'test8',
+            field: 'test8',
+            type: 'array',
+            width: 200,
+            render: {
+              use: true,
+              props: {
+                list: [
+                  { name: 'normal', value: 'normal' },
+                  { name: 'warning', value: 'warning' },
+                  { name: 'critical', value: 'critical' },
+                  { name: 'block', value: 'block' },
+                  { name: 'major', value: 'major' },
+                  { name: 'minor', value: 'minor' },
+                ],
+              },
             },
           },
-        },
-        // { caption: 'test9', field: 'test9', type: 'number' },
-        // { caption: 'test10', field: 'test10', type: 'number' },
-        // { caption: 'test11', field: 'test11', type: 'number' },
-        // { caption: 'test12', field: 'test12', type: 'number' },
-        // { caption: 'test13', field: 'test13', type: 'number' },
-        // { caption: 'test14', field: 'test14', type: 'number' },
-        // { caption: 'test15', field: 'test15' },
-        // { caption: 'test16', field: 'test16' },
-      ],
-      tableData: [],
-    };
-  },
-  watch: {},
-  created() {
-    this.getData(100, 0);
-  },
-  mounted() {
-    // this.getData(100, 100);
-    // setTimeout(this.refreshData.bind(this), 3000);
-  },
-  methods: {
-    onButton() {
-      this.selected = this.tableData[3];
-      for (let ix = 0; ix < this.tableData.length - 5; ix++) {
-        this.checked.push(this.tableData[ix]);
-      }
+          // { caption: 'test9', field: 'test9', type: 'number' },
+          // { caption: 'test10', field: 'test10', type: 'number' },
+          // { caption: 'test11', field: 'test11', type: 'number' },
+          // { caption: 'test12', field: 'test12', type: 'number' },
+          // { caption: 'test13', field: 'test13', type: 'number' },
+          // { caption: 'test14', field: 'test14', type: 'number' },
+          // { caption: 'test15', field: 'test15' },
+          // { caption: 'test16', field: 'test16' },
+        ],
+        tableData: [],
+      };
     },
-    onContextMenu() {
-      console.log('On Context Menu');
-      this.checked = [];
+    watch: {},
+    created() {
+      this.getData(100, 0);
     },
-    checkActive(itemId, row) {
-      if (itemId === 'menu_1') {
-        return row[0] === 1;
-      }
+    mounted() {
+      // this.getData(100, 100);
+      // setTimeout(this.refreshData.bind(this), 3000);
+    },
+    methods: {
+      onButton() {
+        this.selected = this.tableData[3];
+        for (let ix = 0; ix < this.tableData.length - 5; ix++) {
+          this.checked.push(this.tableData[ix]);
+        }
+      },
+      onContextMenu() {
+        console.log('On Context Menu');
+        this.checked = [];
+      },
+      checkActive(itemId, row) {
+        if (itemId === 'menu_1') {
+          return row[0] === 1;
+        }
 
-      return row[1] === 'Russia';
+        return row[1] === 'Russia';
+      },
+      onCheckBox() {
+      },
+      refreshData() {
+        this.getData(100, this.tableData.length);
+        setTimeout(this.refreshData.bind(this), 1000);
+      },
+      getData(count, startIndex) {
+        for (let ix = startIndex; ix < startIndex + count; ix++) {
+          this.tableData.push([
+            ix + 1,
+            countries[ix % 20],
+            Math.random() * 10000,
+            Math.random() * 100,
+            Math.random() * 1000,
+            false,
+            false,
+            'radio-01',
+            null,
+            '',
+            1,
+            'normal',
+            ['warning', 'block'],
+            // Math.random() * 2000,
+            // Math.random() * 3000,
+            // Math.random() * 4000,
+            // Math.random() * 5000,
+            // Math.random() * 6000,
+            // Math.random() * 7000,
+            // Math.random() * 8000,
+            // Math.random() * 9000,
+            // Math.random() * 100,
+            // Math.random() * 200,
+            // Math.random() * 300,
+            // Math.random() * 400,
+            // Math.random() * 500,
+            // Math.random() * 600,
+            // Math.random() * 700,
+            // Math.random() * 800,
+          ]);
+        }
+      },
+      onClick(...parameters) {
+        console.log('Click!!!!');
+        console.log(parameters);
+      },
+      onDblClick(parameters) {
+        console.log('DblClick!!!');
+        console.log(parameters);
+      },
     },
-    onCheckBox() {
-    },
-    refreshData() {
-      this.getData(100, this.tableData.length);
-      setTimeout(this.refreshData.bind(this), 1000);
-    },
-    getData(count, startIndex) {
-      for (let ix = startIndex; ix < startIndex + count; ix++) {
-        this.tableData.push([
-          ix + 1,
-          countries[ix % 20],
-          Math.random() * 10000,
-          Math.random() * 100,
-          Math.random() * 1000,
-          false,
-          false,
-          'radio-01',
-          null,
-          '',
-          1,
-          'normal',
-          ['warning', 'block'],
-          // Math.random() * 2000,
-          // Math.random() * 3000,
-          // Math.random() * 4000,
-          // Math.random() * 5000,
-          // Math.random() * 6000,
-          // Math.random() * 7000,
-          // Math.random() * 8000,
-          // Math.random() * 9000,
-          // Math.random() * 100,
-          // Math.random() * 200,
-          // Math.random() * 300,
-          // Math.random() * 400,
-          // Math.random() * 500,
-          // Math.random() * 600,
-          // Math.random() * 700,
-          // Math.random() * 800,
-        ]);
-      }
-    },
-    onClick(...parameters) {
-      console.log('Click!!!!');
-      console.log(parameters);
-    },
-    onDblClick(parameters) {
-      console.log('DblClick!!!');
-      console.log(parameters);
-    },
-  },
-};
+  };
 </script>
