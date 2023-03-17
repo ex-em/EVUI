d8430d2519810b3a8f06730f48e258c81b8c4720
jinhee park <jinhee@ex-em.com>
2023-03-17 16:56:12 +0900

    [#1375] Chart > tooltip > formatter > html 방식 추가 #####3 - 예제 코드 파일명칭 변경

--- docs/views/lineChart/props.js
+++ docs/views/lineChart/props.js
@@ -12,8 +12,8 @@ import DragSelection from './example/DragSelection';
 import DragSelectionRaw from '!!raw-loader!./example/DragSelection';
 import Tooltip from './example/Tooltip';
 import TooltipRaw from '!!raw-loader!./example/Tooltip';
-import TooltipHtml from './example/TooltipHtml';
-import TooltipHtmlRaw from '!!raw-loader!./example/TooltipHtml';
+import CustomTooltip from './example/CustomTooltip';
+import CustomTooltipRaw from '!!raw-loader!./example/CustomTooltip';
 import PlotLine from './example/PlotLine';
 import PlotLineRaw from '!!raw-loader!./example/PlotLine';
 import SelectLabel from './example/SelectLabel';
@@ -68,8 +68,8 @@ export default {
     },
     'Custom Tooltip': {
       description: 'Tooltip의 내용을 HTML로 가공하여 구성할 수 있습니다.',
-      component: TooltipHtml,
-      parsedData: parseComponent(TooltipHtmlRaw),
+      component: CustomTooltip,
+      parsedData: parseComponent(CustomTooltipRaw),
     },
     'Plot line & Plot band': {
       description: '차트 배경에 선 및 영역을 표시할 수 있습니다.',
