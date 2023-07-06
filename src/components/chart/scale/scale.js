908e2a5736243b2faddd1f15f4e44368d3ca1221
h2yeon <75718910+h2yeon@users.noreply.github.com>
2023-07-06 17:08:15 +0900

    [#1470] Chart > Scatter Chart > Range 사용할 때 Max Value가 그대로 적용되지 않는 이슈 수정 (#1471)
    Co-authored-by: Hye Yeon, Kim <hy2on@ex-em.com>

--- src/components/chart/scale/scale.js
+++ src/components/chart/scale/scale.js
@@ -73,8 +73,13 @@ class Scale {
 
     const range = scrollbarOpt?.use ? scrollbarOpt?.range : this.range;
     if (range?.length === 2) {
-      maxValue = range[1] > +minMax.max ? +minMax.max : range[1];
-      minValue = range[0] < +minMax.min ? +minMax.min : range[0];
+      if (this.options.type === 'heatMap') {
+        maxValue = range[1] > +minMax.max ? +minMax.max : range[1];
+        minValue = range[0] < +minMax.min ? +minMax.min : range[0];
+      } else {
+        maxValue = range[1];
+        minValue = range[0];
+      }
     } else {
       maxValue = minMax.max;
       minValue = minMax.min;
