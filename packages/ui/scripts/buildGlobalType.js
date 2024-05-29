1ab66252c5890166a0fb2e3c967eeee24e93688a
김진범 <jinbekim@ex-em.com>
2024-05-29 22:04:35 +0900

    refactor: Update global.d.ts script to dynamically generate component imports

--- packages/ui/scripts/buildGlobalType.js
+++ packages/ui/scripts/buildGlobalType.js
@@ -1,10 +1,11 @@
 import * as glob from 'glob';
 import * as path from 'path';
 import * as fs from 'fs';
-import { startCase } from 'lodash-es';
+
+const file = './global.d.ts';
 
 fs.writeFileSync(
-  './global.d.ts',
+  file,
   `declare module '@vue/runtime-core' {`,
   { flag: 'w' }
 );
@@ -15,33 +16,33 @@ function getAllComponents() {
 }
 
 fs.writeFileSync(
-  './global.d.ts', `\n  export interface GlobalComponents {`,
+  file, `\n  export interface GlobalComponents {`,
   { flag: 'a' }
 );
 const allComponents = getAllComponents();
 allComponents.forEach((component) => {
   fs.writeFileSync(
-    './global.d.ts',
+    file,
     `\n    Ev${component}: typeof import('@evui/ui')['Ev${component}'];`,
     { flag: 'a' }
   );
 });
-fs.writeFileSync('./global.d.ts', '\n  }', { flag: 'a' });
+fs.writeFileSync(file, '\n  }', { flag: 'a' });
 
 fs.writeFileSync(
-  './global.d.ts',
+  file,
   `\n  export interface ComponentCustomProperties {`,
   { flag: 'a' }
 );
 
 ['message', 'messageBox', 'notification'].forEach((notice) => {
   fs.writeFileSync(
-    './global.d.ts',
+    file,
     `\n    $${notice}: typeof import('@evui/ui')['Ev${notice.charAt(0).toUpperCase() + notice.slice(1)}'];`,
     { flag: 'a' }
   );
 });
 
-fs.writeFileSync('./global.d.ts', `\n  }
+fs.writeFileSync(file, `\n  }
 }
 export {}`, { flag: 'a' });
