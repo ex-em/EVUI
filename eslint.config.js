ee7070b73783a84a6aeb9bcfd477e009151196b4
김진범 <jinbekim@ex-em.com>
2024-06-08 23:28:59 +0900

    add: eslint package

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,3 @@
-import pluginJs from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import eslintConfigPrettier from 'eslint-config-prettier';
-import vuePlugin from 'eslint-plugin-vue';
+import exemConfig from '@evui/eslint-config';
 
-export default tseslint.config(
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...vuePlugin.configs['flat/recommended'],
-  {
-    plugins: {
-      'typescript-eslint': tseslint.plugin,
-    },
-    languageOptions: {
-      parserOptions: {
-        parser: tseslint.parser,
-        extraFileExtensions: ['.vue'],
-        sourceType: 'module',
-      },
-    },
-  },
-  {
-    ignores: ['**/dist/', '**/node_modules/'],
-  },
-  eslintConfigPrettier
-);
+export default [...exemConfig];
