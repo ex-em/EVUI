018b97ca6b2ce57fcbe885997a785f44c17068a4
이인구 <in9@ex-em.com>
2024-07-22 17:56:42 +0900

    [#1714] Tree Grid > 마지막 컬럼 사이즈 조절 버그
    #######################
    
    - 컬럼 사이즈 변경 시 모든 컬럼들의 resized 속성이 true로 변경되는 현상을 수정했습니다.

--- src/components/treeGrid/uses.js
+++ src/components/treeGrid/uses.js
@@ -343,11 +343,7 @@ export const resizeEvent = (params) => {
 
       if (stores.orderedColumns[columnIndex]) {
         stores.orderedColumns[columnIndex].width = changedWidth;
-        stores.orderedColumns.forEach((column) => {
-          const item = column;
-          item.resized = true;
-          return item;
-        });
+        stores.orderedColumns[columnIndex].resized = true;
       }
 
       resizeInfo.showResizeLine = false;
