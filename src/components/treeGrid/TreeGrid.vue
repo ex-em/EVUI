679208dc75ab744f6edb804d3024609eceec2205
오지영 <jiyoung.oh@ex-em.com>
2024-08-06 09:26:53 +0900

    [#1727]Grid > contextMenuClass 옵션 추가, ContextMenu > hidden, itemClass 옵션 추가 ################################ [작업 내용] - EvTreeGrid에 customContextMenuClass 옵션 추가

--- src/components/treeGrid/TreeGrid.vue
+++ src/components/treeGrid/TreeGrid.vue
@@ -191,6 +191,7 @@
         <!-- Context Menu -->
         <ev-context-menu
           ref="menu"
+          :custom-class="contextMenuClass"
           :items="contextMenuItems"
         />
         <ev-context-menu
@@ -452,6 +453,7 @@ export default {
     const contextInfo = reactive({
       menu: null,
       contextMenuItems: [],
+      contextMenuClass: props.option.customContextMenuClass || '',
       columnMenu: null,
       columnMenuItems: [],
       columnMenuTextInfo: props.option.columnMenuText || {},
