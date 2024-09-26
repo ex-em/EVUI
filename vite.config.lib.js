3604ef95fcfc92a9bd927410f06be865ddb1368e
김진범 <jinbekim@ex-em.com>
2024-09-26 16:22:44 +0900

    feat: build mode

--- vite.config.lib.js
+++ vite.config.lib.js
@@ -5,6 +5,21 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/main.js'),
+      name: 'evui',
+      fileName: 'evui',
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
   plugins: [vue()],
   resolve: {
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
