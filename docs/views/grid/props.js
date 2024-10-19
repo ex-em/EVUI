53fa1b9b115fd423526da53519a4c90c242b01f6
김진범 <jinbekim@ex-em.com>
2024-10-19 19:26:26 +0900

    fix: scatter chart

--- docs/views/grid/props.js
+++ docs/views/grid/props.js
@@ -1,32 +1,31 @@
-import { parse } from '@vue/compiler-sfc';
-import ColumnSetting from 'docs/views/grid/example/ColumnSetting.vue';
-import mdText from './api/grid.md?raw';
-import ColumnSettingRaw from './example/ColumnSetting.vue?raw';
-import Default from './example/Default';
-import DefaultRaw from './example/Default?raw';
-import CellRenderer from './example/CellRenderer';
-import CellRendererRaw from './example/CellRenderer?raw';
-import Toolbar from './example/Toolbar';
-import ToolbarRaw from './example/Toolbar?raw';
-import Summary from './example/Summary';
-import SummaryRaw from './example/Summary?raw';
-import RowDetail from './example/RowDetail.vue';
-import RowDetailRaw from './example/RowDetail.vue?raw';
-import Sort from './example/Sort.vue';
-import SortRaw from './example/Sort.vue?raw';
-import ColumnEvent from './example/ColumnEvent.vue';
-import ColumnEventRaw from './example/ColumnEvent.vue?raw';
-import Disabled from './example/Disabled.vue';
-import DisabledRaw from './example/Disabled.vue?raw';
-import CustomHeader from './example/CustomHeader.vue';
-import CustomHeaderRaw from './example/CustomHeader.vue?raw';
-import DisabledRaw from './example/Disabled.vue?raw';
+import { parse } from "@vue/compiler-sfc";
+import ColumnSetting from "docs/views/grid/example/ColumnSetting.vue";
+import mdText from "./api/grid.md?raw";
+import ColumnSettingRaw from "./example/ColumnSetting.vue?raw";
+import Default from "./example/Default";
+import DefaultRaw from "./example/Default?raw";
+import CellRenderer from "./example/CellRenderer";
+import CellRendererRaw from "./example/CellRenderer?raw";
+import Toolbar from "./example/Toolbar";
+import ToolbarRaw from "./example/Toolbar?raw";
+import Summary from "./example/Summary";
+import SummaryRaw from "./example/Summary?raw";
+import RowDetail from "./example/RowDetail.vue";
+import RowDetailRaw from "./example/RowDetail.vue?raw";
+import Sort from "./example/Sort.vue";
+import SortRaw from "./example/Sort.vue?raw";
+import ColumnEvent from "./example/ColumnEvent.vue";
+import ColumnEventRaw from "./example/ColumnEvent.vue?raw";
+import Disabled from "./example/Disabled.vue";
+import DisabledRaw from "./example/Disabled.vue?raw";
+import CustomHeader from "./example/CustomHeader.vue";
+import CustomHeaderRaw from "./example/CustomHeader.vue?raw";
 
 export default {
   mdText,
   components: {
     Default: {
-      description: '그리드 컴포넌트입니다.',
+      description: "그리드 컴포넌트입니다.",
       component: Default,
       parsedData: parse(DefaultRaw).descriptor,
     },
@@ -54,7 +53,7 @@ export default {
       component: ColumnEvent,
       parsedData: parse(ColumnEventRaw).descriptor,
     },
-    'Custom Column List': {
+    "Custom Column List": {
       component: ColumnSetting,
       parsedData: parse(ColumnSettingRaw).descriptor,
     },
@@ -62,9 +61,9 @@ export default {
       component: Disabled,
       parsedData: parse(DisabledRaw).descriptor,
     },
-    'Custom Table Header': {
+    "Custom Table Header": {
       component: CustomHeader,
-      parsedData: parseComponent(CustomHeaderRaw),
+      parsedData: parse(CustomHeaderRaw).descriptor,
     },
   },
 };
