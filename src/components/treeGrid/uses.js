bd10c3cb5ddb5a2350c4323b6d1743c292506059
jinhee park <jinhee@ex-em.com>
2024-11-09 18:40:18 +0900

    [#1808]Tree Grid > sorting 기능 임시 잠금 ###################################### - column 데이터 받을 때 sortOptions이 잇을 경우 화면에 바로 보여지도록 함

--- src/components/treeGrid/uses.js
+++ src/components/treeGrid/uses.js
@@ -1028,7 +1028,7 @@ export const sortEvent = ({ sortInfo, stores }) => {
 
 
   const setSortInfo = (columns, emitTriggered = true) => {
-    const sortByColumn = columns?.find(col => col?.sortOption?.sortType && col.sortOption?.sortType !== 'init');
+    const sortByColumn = columns?.find(col => col?.sortOption?.sortType && col.sortOption.sortType !== 'init');
     const { sortType } = sortByColumn?.sortOption || {};
 
     sortInfo.sortColumn = sortByColumn;
