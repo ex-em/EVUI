7ce7d43ba8cc0548c45336b7a54401a36a19595f
김진범 <jinbekim@ex-em.com>
2025-01-03 10:01:57 +0900

    fix: example

--- docs/views/barChart/example/Default.vue
+++ docs/views/barChart/example/Default.vue
@@ -14,7 +14,7 @@
         },
         labels: ['가나다라', '마바사', '아자차', '카타', '파하'],
         data: {
-          series1: [100, 150, 51, 150, 450],
+          series1: [100, 150, 51, 150, 350],
         },
       };
 
@@ -28,7 +28,7 @@
           showAxis: true,
           type: 'linear',
           startToZero: true,
-          range: (min, max) => (max > 300 ? [0, max] : [0, 300]),
+          autoScaleRatio: 0.1,
         }],
       };
 
