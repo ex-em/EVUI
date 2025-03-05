f4a76097dfe3f53db71ea88daf5b1b45c899d657
jiwlee <ddaezi9576@ex-em.com>
2025-03-05 17:35:47 +0900

    feat: 클릭 이벤트에 label, dataIndex 추가

--- src/components/chart/plugins/plugins.interaction.js
+++ src/components/chart/plugins/plugins.interaction.js
@@ -177,6 +177,7 @@ const modules = {
       };
 
       const setSelectedLabelInfo = (targetAxis) => {
+        const itemHitInfo = this.getItemByPosition(offset, false);
         const {
           labelIndex: clickedLabelIndex,
         } = this.getLabelInfoByPosition(offset, targetAxis);
@@ -195,10 +196,14 @@ const modules = {
           eventTarget: 'label',
           ...cloneDeep(this.defaultSelectInfo),
         };
+        args.label = itemHitInfo.label;
+        args.dataIndex = itemHitInfo.maxIndex;
       };
 
       const setSelectedSeriesInfo = () => {
+        const itemHitInfo = this.getItemByPosition(offset, false);
         const hitInfo = this.getSeriesInfoByPosition(offset);
+
         if (hitInfo.sId !== null) {
           const allSelectedList = this.updateSelectedSeriesInfo(hitInfo.sId);
           this.defaultSelectInfo.seriesId = allSelectedList.seriesId;
@@ -207,6 +212,8 @@ const modules = {
             eventTarget: 'series',
             ...cloneDeep(this.defaultSelectInfo),
           };
+          args.label = itemHitInfo.label;
+          args.dataIndex = itemHitInfo.maxIndex;
         }
       };
 
