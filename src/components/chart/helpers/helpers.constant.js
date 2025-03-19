85fa0da525a778b3e1cd1d28091eeb868ddbbc30
박진희 <53548023+jhee564@users.noreply.github.com>
2025-03-19 00:29:11 -0700

    [#1889] feat: Heatmap > highlight 스타일 옵션화 (#1892)
    * [#1889] feat: Heatmap > highlight 스타일 옵션화
    1. heatmap chart > highlight 스타일을 변경할 수 있는 옵션 추가
    2. 관련 예제 파일 추가 (HighlightStyle)
    
    * [#1889] feat: Heatmap > highlight 스타일 옵션화
    1. 관련 설명 추가 및 esLint error 픽스
    
    * [#1889] feat: Heatmap > highlight 스타일 옵션화
    1. esLint error 픽스
    
    * version update (3.4.93 -> 3.4.94)

--- src/components/chart/helpers/helpers.constant.js
+++ src/components/chart/helpers/helpers.constant.js
@@ -157,10 +157,6 @@ export const PLOT_BAND_OPTION = {
 
 export const HEAT_MAP_OPTION = {
   show: true,
-  highlight: {
-    maxShadowOpacity: 0.4,
-    brightness: 150,
-  },
   xAxisIndex: 0,
   yAxisIndex: 0,
   showLegend: true,
@@ -170,6 +166,21 @@ export const HEAT_MAP_OPTION = {
     textColor: '#000000',
     formatter: null,
     decimalPoint: 0,
+  },
+  highlight: {
+    stroke: {
+      use: false,
+      color: null,
+      width: 1,
+      radius: 0,
+    },
+    shadow: {
+      use: true,
+      offsetX: 0,
+      offsetY: 0,
+      blur: 4,
+      color: '#959494',
+    },
   },
 };
 
