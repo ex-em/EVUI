eb2b7420d9cc78aa05c9c2e0d5037c5652c726f7
jiwlee <ddaezi9576@ex-em.com>
2025-07-04 14:45:25 +0900

    refactor: update TypeScript configuration and replace Vite config with Vitest config; add unit tests for BigNumber and utility functions

--- test-env/packages/utils/bignumber.spec.js
+++ test-env/packages/utils/bignumber.spec.js
@@ -1,3 +1,4 @@
+import { describe, test, expect } from 'vitest';
 import BigNumber from 'bignumber.js';
 import {
   toBigNumber,
--- test-env/packages/utils/utils.spec.js
+++ test-env/packages/utils/utils.spec.js
@@ -1,3 +1,4 @@
+import { describe, test, expect, beforeEach, afterAll } from 'vitest';
 import {
   getQuantity,
   truthyNumber,
