487a9cf663c467c52b4ab96804f54325e5e4110f
박시선 <95465735+pparkssi3@users.noreply.github.com>
2025-08-05 16:50:42 +0900

    fix: grid width 숫자 타입 아닐때 기본값 처리 (#1952)

--- src/components/treeGrid/TreeGrid.vue
+++ src/components/treeGrid/TreeGrid.vue
@@ -455,6 +455,7 @@ export default {
         index,
         hiddenDisplay: false,
         ...column,
+        width: isFinite(column.width) ? column.width : undefined,
         sortOption: {
            sortType: column?.sortOption?.sortType || 'init',
         },
