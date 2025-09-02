3ed81f0965e9f5f102c9c4d8e2475599305b9bcb
김진범 <jinbekim@ex-em.com>
2025-09-02 14:49:57 +0900

    fix: tooltip 사용 시에만 indicator가 데이터 포인트 위치에 표시되도록 수정
    - tooltip.use가 true일 때만 데이터 포인트 위치 사용
    - tooltip이 비활성화된 경우 기존처럼 마우스 커서 위치에 indicator 표시

--- src/components/chart/plugins/plugins.interaction.js
+++ src/components/chart/plugins/plugins.interaction.js
@@ -79,9 +79,9 @@ const modules = {
       }
 
       if (indicator.use && type !== 'pie' && type !== 'scatter' && type !== 'heatMap') {
-        // Use data point position instead of mouse position for indicator
+        // Use data point position instead of mouse position for indicator when tooltip is enabled
         let indicatorOffset = offset;
-        if (Object.keys(hitInfo.items).length) {
+        if (tooltip.use && Object.keys(hitInfo.items).length) {
           const hitId = hitInfo.hitId || Object.keys(hitInfo.items)[0];
           const hitItem = hitInfo.items[hitId];
           if (hitItem && hitItem.data && hitItem.data.xp !== undefined
