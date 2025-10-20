9789e1f2b79bb0623830a4ae9c31ec4f9f838d92
김진범 <59330110+jinbekim@users.noreply.github.com>
2025-10-20 18:14:50 +0900

    feat: 아이콘에 컬러속성을 지정하면 아이콘의 색상을 변경할 수 있습니다. (#2033)
    * feat: enhance icon component with color and size props; update examples in Default.vue to demonstrate new features
    
    * Fix spacing in icon class selector
    
    * feat: add color prop to icon component documentation and update example to reflect new color options

--- docs/views/icon/example/Default.vue
+++ docs/views/icon/example/Default.vue
@@ -4,29 +4,37 @@
     <div style="font-size: 40px;">
       <ev-icon icon="ev-icon-user" />
       <ev-icon icon="ev-icon-topology" />
-      <i class="ev-icon-server"/>
+      <i class="ev-icon-server" />
     </div>
     <div class="description" style="margin-bottom: 20px;">
       font-size 지정하여 크기 변경
     </div>
-    <div>
-      <ev-icon
-        icon="ev-icon-user2"
-        size="small"
-      />
-      <ev-icon
-        icon="ev-icon-user2"
-        size="medium"
-      />
-      <ev-icon
-        icon="ev-icon-user2"
-        size="large"
-      />
+  </div>
+
+  <div class="case">
+    <p class="case-title">Color</p>
+    <div style="font-size: 40px;">
+      <ev-icon icon="ev-icon-user2" color="red" />
+      <ev-icon icon="ev-icon-user2" color="#0000ff" />
+      <ev-icon icon="ev-icon-user2" color="green" />
+    </div>
+    <div class="description" style="margin-bottom: 20px;">
+      color 변수(red, #ffffff, ...) 바인딩
+    </div>
+  </div>
+
+  <div class="case">
+    <p class="case-title">Size</p>
+    <div style="font-size: 40px;">
+      <ev-icon icon="ev-icon-user2" size="small" />
+      <ev-icon icon="ev-icon-user2" size="medium" />
+      <ev-icon icon="ev-icon-user2" size="large" />
     </div>
     <div class="description">
       size 변수(small, medium, large) 바인딩
     </div>
   </div>
+
   <div class="case">
     <p class="case-title">Event</p>
     <div style="font-size: 40px;">
