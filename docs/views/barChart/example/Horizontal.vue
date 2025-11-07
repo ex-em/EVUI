fc338feba46341ccfc06cf61dde061a30faa9d4b
jinhee <jinhee@ex-em.com>
2025-11-07 15:29:55 +0900

    테스트 코드 원복

--- docs/views/barChart/example/Horizontal.vue
+++ docs/views/barChart/example/Horizontal.vue
@@ -8,13 +8,28 @@
 <script>
   export default {
     setup() {
+      const addUnit = (value) => {
+        const bytes = +value;
+
+        if (bytes === 0) {
+          return '0 Bytes';
+        }
+
+        const decimalPoint = 2;
+        const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+        const k = 1024;
+        const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+        return `${parseFloat((bytes / k ** i).toFixed(decimalPoint))} ${sizes[i]}`;
+      };
+
       const chartData = {
         series: {
-          series1: { name: 'series#1', showValue: { use: true, fontSize: 12, textColor: '#000000', align: 'out', decimalPoint: null } },
+          series1: { name: 'series#1', showValue: { use: true, fontSize: 12, textColor: '#000000', align: 'out', formatter: addUnit } },
         },
         labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
         data: {
-          series1: [0, 0, 0, 0, 0.2],
+          series1: [1500.123456, 180.123456, 30.123456, 10, 0],
         },
       };
 
@@ -38,6 +53,7 @@
           startToZero: true,
           autoScaleRatio: 0.1,
           showGrid: true,
+          formatter: addUnit,
         }],
         axesY: [{
           type: 'step',
