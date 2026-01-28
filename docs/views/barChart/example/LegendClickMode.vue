08e212f361710401d968fa6449fe6e36b1cebd3b
hyeonbbang <hy2on@ex-em.com>
2026-01-28 16:08:55 +0900

    feat: add click event handler to legend in bar chart example

--- docs/views/barChart/example/LegendClickMode.vue
+++ docs/views/barChart/example/LegendClickMode.vue
@@ -17,7 +17,7 @@
         inactive, 처음 클릭시 해당 시리즈만 감춤, 마지막 남은 범례 클릭시 무시
       </p>
     </div>
-    <ev-chart :data="chartData" :options="chartOptions" />
+    <ev-chart :data="chartData" :options="chartOptions" @click-legend="handleClickLegend" />
     <div class="result">
       <div class="badge yellow">클릭된 시리즈 ID</div>
       {{ clickedSeriesIds }}
