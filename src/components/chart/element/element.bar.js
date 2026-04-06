3a788ef1ff3ed492b0ccd41741526902f4c423e2
이지원 <ddaezi9576@ex-em.com>
2026-04-06 15:29:16 +0900

    feat: bar element에 directHit 플래그 추가 -- element.bar
    bar의 findGraphData와 binarySearchBar가 isPointInBar로 박스 히트를
    판정할 때, 그 결과를 item.directHit 플래그로도 내려준다.
    findHitItem에서 line 포인트 근접 히트보다 박스 직접 히트를
    우선하도록 하기 위한 사전 작업.

--- src/components/chart/element/element.bar.js
+++ src/components/chart/element/element.bar.js
@@ -301,6 +301,9 @@ class Bar {
         item.data = barData[clampedIndex];
         item.index = clampedIndex;
         item.hit = this.isPointInBar(offset, barData[clampedIndex]);
+        // bar 박스 내부 클릭은 "직접 박스 히트"로 표시.
+        // findHitItem에서 line 포인트 근접 히트보다 우선 선택되도록 하기 위함.
+        item.directHit = item.hit;
       }
 
       return item;
@@ -347,6 +350,8 @@ class Bar {
         item.data = barData;
         item.index = barData.index;
         item.hit = this.isPointInBar(offset, barData);
+        // bar 박스 내부 클릭은 "직접 박스 히트"로 표시 (findHitItem 우선순위용).
+        item.directHit = item.hit;
         return item;
       }
 
