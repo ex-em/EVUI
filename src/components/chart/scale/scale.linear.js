992458b4954aeb4b8b54abc4ba5988f87a49a3a0
hyeonbbang <hy2on@ex-em.com>
2026-04-16 10:42:22 +0900

    fix: interval이 range를 벗어나는 경우 로직 타지 않게 수정

--- src/components/chart/scale/scale.linear.js
+++ src/components/chart/scale/scale.linear.js
@@ -167,7 +167,7 @@ class LinearScale extends Scale {
       const pow = 10 ** exp;
       for (const fraction of NICE_FRACTIONS) {
         const interval = fraction * pow;
-        if (interval >= minInterval) {
+        if (interval >= minInterval && interval < range) {
           // floating-point 오차 보정을 위해 epsilon 적용
           const EPS = interval * 1e-10;
           const niceMin = Math.floor((min + EPS) / interval) * interval;
@@ -424,7 +424,7 @@ class LinearScale extends Scale {
   
       while (steps > maxSteps) {
         interval += resolvedInterval;
-  
+
         ({ graphMin, graphMax } = expandByInterval({
           min: minValue,
           max: maxValue,
