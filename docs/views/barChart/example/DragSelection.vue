8bcf446be5d4b60c7390da69a82877974bb81b6b
손기연 <sonky740@ex-em.com>
2026-08-21 13:58:18 +0900

    docs(chart): bar 차트 drag-select 예제·API 문서 추가
    barChart 에 DragSelection 예제를 추가한다(line 과 같은 구성, time +
    categoryMode 축). barChart.md 에도 options.dragSelection 상세와 Event 표의
    drag-select 행이 빠져 있어 barChart.json 과 맞춘다.
    
    range.xMin/xMax 는 x축 최소·최대값 사이의 선형 보간이라 문자 라벨을 쓰는 step
    축에서는 NaN 이 된다. bar 는 step 축이 가장 흔해서 문서에 명시했다(차트 타입
    공통 동작).

---
+++ docs/views/barChart/example/DragSelection.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="case">
+    <resizable-wrapper>
+      <ev-chart :data="chartData" :options="chartOptions" @drag-select="onDragSelect" />
+    </resizable-wrapper>
+  </div>
+  <div class="description">
+    <div class="badge yellow">범위 값</div>
+    <br /><br />
+    <div v-if="selectionRange.xMin">
+      <p>X min : {{ convertToDateString(selectionRange.xMin) }}</p>
+      <p>X max : {{ convertToDateString(selectionRange.xMax) }}</p>
+      <p>Y min : {{ selectionRange.yMin }}</p>
+      <p>Y max : {{ selectionRange.yMax }}</p>
+    </div>
+    <br />
+    <div class="badge yellow">선택 영역 내 데이터</div>
+    <br /><br />
+    <p>
+      바 시리즈는 <i>data</i> 에 담기지 않는다({{ selectionItems.length }}건). 범위는 위 range 로
+      판단한다.
+    </p>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+import dayjs from 'dayjs';
+
+export default {
+  setup() {
+    const startTime = dayjs().startOf('hour').subtract(23, 'hour');
+    const labels = [];
+    const series1 = [];
+    const series2 = [];
+
+    for (let ix = 0; ix < 24; ix++) {
+      labels.push(startTime.add(ix, 'hour'));
+      series1.push(Math.floor(Math.random() * 100) + 10);
+      series2.push(Math.floor(Math.random() * 100) + 10);
+    }
+
+    const chartData = {
+      series: {
+        series1: { name: 'series#1' },
+        series2: { name: 'series#2' },
+      },
+      labels,
+      data: {
+        series1,
+        series2,
+      },
+    };
+
+    const chartOptions = {
+      type: 'bar',
+      width: '100%',
+      thickness: 0.8,
+      title: {
+        text: 'Chart Title',
+        show: true,
+      },
+      legend: {
+        show: true,
+        position: 'right',
+      },
+      horizontal: false,
+      axesX: [
+        {
+          type: 'time',
+          categoryMode: true,
+          showGrid: false,
+          timeFormat: 'HH:mm',
+          interval: 'hour',
+        },
+      ],
+      axesY: [
+        {
+          type: 'linear',
+          startToZero: true,
+          autoScaleRatio: 0.1,
+          showGrid: true,
+        },
+      ],
+      dragSelection: {
+        use: true,
+        keepDisplay: true,
+      },
+    };
+
+    const selectionItems = ref([]);
+    const selectionRange = ref({});
+    const onDragSelect = ({ data, range }) => {
+      selectionItems.value = data;
+      selectionRange.value = range;
+    };
+
+    const convertToDateString = (value) => dayjs(value).format('MM/DD HH:mm');
+
+    return {
+      chartData,
+      chartOptions,
+      selectionItems,
+      selectionRange,
+      onDragSelect,
+      convertToDateString,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.description {
+  span {
+    margin-right: 15px;
+  }
+}
+</style>
